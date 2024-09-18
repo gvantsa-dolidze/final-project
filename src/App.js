c8ac56d1f0734564b31f1755fdea289dc2c3e6f4
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-18 22:23:39 +0400

    in App.js import Home.jsx and Add routing for it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
          <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+
+<Routes>
+  <Route path="/" element={ <Home /> } />
+</Routes>
     </div>
   );
 }
