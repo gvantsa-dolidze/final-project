cd97c018baa4f996b73d9c2adaa0828b84d419b6
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-23 17:15:20 +0400

    create header and footer component and import inn App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import './App.css';
-import Home from './pages/Home';
-import { Route, Routes } from 'react-router-dom';
+import "./App.css";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from './components/Footer';
+import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-         <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-<Routes>
-  <Route path="/" element={ <Home /> } />
-</Routes>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
