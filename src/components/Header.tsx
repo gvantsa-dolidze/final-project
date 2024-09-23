db7fea818d3b147a7feb18c4bfb80c4e1552bf27
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-23 20:16:00 +0400

    create HeaderLogo component and import in Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import HeaderLogo from "./elements/HeaderLogo"
 import PromoHeader from "./elements/PromoHeader"
 const Header = () => {
   return (
     <div>
       <PromoHeader />
-      
+      <div>
+<HeaderLogo />
+
+      </div>
     </div>
   )
 }
---
+++ src/components/elements/HeaderLogo.tsx
@@ -0,0 +1,9 @@
+const HeaderLogo = () => {
+  return (
+    <div className="flex items-center ">
+        <img src="/img/logo/Logomark.png" alt="logo" /> Ecommerce
+    </div>
+  )
+}
+
+export default HeaderLogo
