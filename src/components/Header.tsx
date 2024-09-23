a158682cf42ac447d30d0b78b5bfaedfa75f4421
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-23 22:56:15 +0400

    change header style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,21 @@
-import HeaderLogo from "./elements/HeaderLogo";
-import HeaderNav from "./elements/HeaderNav";
-import LoginAndCart from "./elements/LoginAndCart";
-import PromoHeader from "./elements/PromoHeader";
-import SearchBar from "./elements/SearchBar";
+import HeaderLogo from "./header_elements/HeaderLogo";
+import HeaderNav from "./header_elements/HeaderNav";
+import LoginAndCart from "./header_elements/LoginAndCart";
+import PromoHeader from "./header_elements/PromoHeader";
+import SearchBar from "./header_elements/SearchBar";
 const Header = () => {
   return (
     <div>
       <PromoHeader />
       <div className="flex items-center space-x-28 max-w-1116 m-auto pt-3 pb-3">
-        <HeaderLogo />
-        <HeaderNav />
+        <div className="flex items-center gap-[116px]">
+          <HeaderLogo />
+          <HeaderNav />
+        </div>
+
         <SearchBar />
         <LoginAndCart />
       </div>
-      
     </div>
   );
 };
