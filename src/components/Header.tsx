d9a3f4a1f1e8aa367b4a2803493564f69a0d225b
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-23 23:19:34 +0400

    update styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div>
       <PromoHeader />
-      <div className="flex items-center space-x-28 max-w-1116 m-auto pt-3 pb-3">
+      <div className="flex items-center space-x-28 max-w-1116 m-auto pt-3 pb-3 justify-between">
         <div className="flex items-center gap-[116px]">
           <HeaderLogo />
           <HeaderNav />
--- src/components/header_elements/LoginAndCart.tsx
+++ src/components/header_elements/LoginAndCart.tsx
@@ -1,11 +1,16 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
 const LoginAndCart = () => {
   return (
-    <div className="flex">
-        <NavLink to='/'><img src="/img/icons/cart.png" alt="login" /></NavLink>
-        <NavLink to='/'><img src="/img/icons/user.png" alt="cart" /></NavLink>
-    </div>
-  )
-}
+    <div className="flex gap-8 shrink-0 items-center">
+      <NavLink to="/">
+        <img src="/img/icons/cart.png" alt="login"/>
+      </NavLink>
 
-export default LoginAndCart
+      <NavLink to="/">
+        <img src="/img/icons/user.png" alt="cart"/>
+      </NavLink>
+    </div>
+  );
+};
+
+export default LoginAndCart;
