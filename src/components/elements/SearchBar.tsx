9bbd7fc505401e696f20383c01530a3c5e9bc5e8
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-23 21:58:29 +0400

    - Created SearchBar component with an input field and a search icon

---
+++ src/components/elements/SearchBar.tsx
@@ -0,0 +1,13 @@
+const SearchBar = () => {
+  return (
+    <form>
+        <div className="flex items-center">
+            <img src="/img/icons/Search.png" alt="" />
+            <input type="text" placeholder="" />
+        </div>
+        
+    </form>
+  )
+}
+
+export default SearchBar
