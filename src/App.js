cd19f3ec38154a24a101017bc25515d7a4cb37d2
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-23 18:49:44 +0400

    create CheckoutPage.tsx and import in App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from './components/Footer';
 import ProductPage from './pages/ProductPage';
 import ListingPage from './pages/ListingPage';
 import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
 import { Route, Routes } from "react-router-dom";
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
         <Route path="/product_page" element={<ProductPage />} />
         <Route path="/listing_page" element={<ListingPage />} />
         <Route path="/cart_page" element={<CartPage />} />
+        <Route path="/checkout_page" element={<CheckoutPage />} />
+
 
       </Routes>
       <Footer />
