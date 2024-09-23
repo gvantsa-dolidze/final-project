3aba0478cca7a4f73d45f4f1eb198c89a397f255
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-23 21:48:18 +0400

    add arrow down img to Categories link

--- src/components/elements/HeaderNav.tsx
+++ src/components/elements/HeaderNav.tsx
@@ -2,9 +2,9 @@ import { NavLink } from "react-router-dom"
 
 const HeaderNav = () => {
   return (
-    <div className="space-x-5">
+    <div className="space-x-5 flex">
         <NavLink to='/'>Home</NavLink>
-        <NavLink to='/'>Categories</NavLink>
+        <NavLink to='/' className='flex'>Categories<img src="/img/icons/Chevron Down.png" alt="arrow_down" /></NavLink>
         <NavLink to='/about'>About</NavLink>
         <NavLink to='/contact'>Contact</NavLink>
     </div>
