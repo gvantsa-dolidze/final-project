12aad6a7f0668106107596d50df8952b982c1adb
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-23 22:09:17 +0400

    add placeholder

--- src/components/elements/SearchBar.tsx
+++ src/components/elements/SearchBar.tsx
@@ -3,7 +3,7 @@ const SearchBar = () => {
     <form>
         <div className="flex items-center">
             <img src="/img/icons/Search.png" alt="" />
-            <input type="text" placeholder="" className="outline-none"/>
+            <input type="text" placeholder="Search products" className="outline-none"/>
         </div>
         
     </form>
