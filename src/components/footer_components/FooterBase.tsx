b03f25fd302336bd2c509f0a798604daa151a93b
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-25 16:40:29 +0400

    add justify-between in FooterBase Styles

--- src/components/footer_components/FooterBase.tsx
+++ src/components/footer_components/FooterBase.tsx
@@ -5,7 +5,7 @@ import FooterNav from "./FooterNav";
 
 const FooterBase = () => {
   return (
-    <div className="max-w-1116 flex m-auto">
+    <div className="max-w-1116 flex justify-between m-auto">
       <FooterLogo />
       <FooterNav />
       <AcceptedPayments />
