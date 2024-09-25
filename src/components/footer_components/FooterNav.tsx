24cb3127efa53c1760b8fec37a378cb40171fb3c
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-25 19:19:05 +0400

    Implement FooterNav component with navigation links for support, company, and shop sections.

--- src/components/footer_components/FooterNav.tsx
+++ src/components/footer_components/FooterNav.tsx
@@ -1,8 +1,34 @@
+import { NavLink } from "react-router-dom";
 
 const FooterNav = () => {
   return (
-    <div>FooterNav</div>
-  )
-}
+    <div className="flex space-x-10 text-start">
+      <div>
+        <h2>SUPPORT</h2>
+        <nav className="flex flex-col mt-10 gap-2">
+          <NavLink to="/">FAQ</NavLink>
+          <NavLink to="/">Terms of use</NavLink>
+          <NavLink to="/">Privacy Policy</NavLink>
+        </nav>
+      </div>
+      <div>
+        <h2>COMPANY</h2>
+        <nav className="flex flex-col mt-10 gap-2">
+          <NavLink to="/">About us</NavLink>
+          <NavLink to="/">Contact</NavLink>
+          <NavLink to="/">Careers</NavLink>
+        </nav>
+      </div>
+      <div>
+        <h2>SHOP</h2>
+        <nav className="flex flex-col mt-10 gap-2">
+          <NavLink to="/">My Account</NavLink>
+          <NavLink to="/">Checkout</NavLink>
+          <NavLink to="/">Cart</NavLink>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
-export default FooterNav
+export default FooterNav;
