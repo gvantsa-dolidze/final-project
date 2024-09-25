57946427751f943c10f15afcea486c4077632782
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-25 21:50:58 +0400

    Refactor FooterNav component to use props for dynamic content instead of hardcoded text

--- src/components/footer_components/FooterBase.tsx
+++ src/components/footer_components/FooterBase.tsx
@@ -2,17 +2,27 @@ import AcceptedPayments from "./AcceptedPayments";
 import FooterLogo from "./FooterLogo";
 import FooterNav from "./FooterNav";
 
-
 const FooterBase = () => {
   return (
     <div className="max-w-1116 flex justify-between m-auto pt-14 pb-14">
       <FooterLogo />
-      <FooterNav />
+      <FooterNav
+        support="SUPPORT"
+        faq="FAQ"
+        termsOfUse="Terms of use"
+        privacyPolicy="Privacy Policy"
+        company="COMPANY"
+        aboutUs="About us"
+        contact="Contact"
+        careers="Careers"
+        shop="SHOP"
+        myAccount="My Account"
+        checkout="Checkout"
+        cart="Cart"
+      />
       <AcceptedPayments />
-      
     </div>
-  )
-  
+  );
 };
 
 export default FooterBase;
--- src/components/footer_components/FooterNav.tsx
+++ src/components/footer_components/FooterNav.tsx
@@ -1,30 +1,58 @@
 import { NavLink } from "react-router-dom";
 
-const FooterNav = () => {
+interface PromoHeaderProps {
+  support: string;
+  faq: string;
+  termsOfUse: string;
+  privacyPolicy: string;
+  company: string;
+  aboutUs: string;
+  contact: string;
+  careers: string;
+  shop: string;
+  myAccount: string;
+  checkout: string;
+  cart: string;
+}
+
+const FooterNav: React.FC<PromoHeaderProps> = ({
+  support,
+  faq,
+  termsOfUse,
+  privacyPolicy,
+  company,
+  aboutUs,
+  contact,
+  careers,
+  shop,
+  myAccount,
+  checkout,
+  cart,
+}) => {
   return (
     <div className="flex space-x-10 text-start">
       <div>
-        <h2>SUPPORT</h2>
+        <h2>{support}</h2>
         <nav className="flex flex-col mt-10 gap-2">
-          <NavLink to="/">FAQ</NavLink>
-          <NavLink to="/">Terms of use</NavLink>
-          <NavLink to="/">Privacy Policy</NavLink>
+          <NavLink to="/">{faq}</NavLink>
+          <NavLink to="/">{termsOfUse}</NavLink>
+          <NavLink to="/">{privacyPolicy}</NavLink>
         </nav>
       </div>
       <div>
-        <h2>COMPANY</h2>
+        <h2>{company}</h2>
         <nav className="flex flex-col mt-10 gap-2">
-          <NavLink to="/">About us</NavLink>
-          <NavLink to="/">Contact</NavLink>
-          <NavLink to="/">Careers</NavLink>
+          <NavLink to="/">{aboutUs}</NavLink>
+          <NavLink to="/">{contact}</NavLink>
+          <NavLink to="/">{careers}</NavLink>
         </nav>
       </div>
       <div>
-        <h2>SHOP</h2>
+        <h2>{shop}</h2>
         <nav className="flex flex-col mt-10 gap-2">
-          <NavLink to="/">My Account</NavLink>
-          <NavLink to="/">Checkout</NavLink>
-          <NavLink to="/">Cart</NavLink>
+          <NavLink to="/">{myAccount}</NavLink>
+          <NavLink to="/">{checkout}</NavLink>
+          <NavLink to="/">{cart}</NavLink>
         </nav>
       </div>
     </div>
