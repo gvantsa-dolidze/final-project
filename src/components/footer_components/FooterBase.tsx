bad52012ec8a1c2df6640808aaa7c63545447579
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-25 16:37:41 +0400

    Import FooterLogo, FooterNav, and AcceptedPayments components into FooterBase

--- src/components/footer_components/FooterBase.tsx
+++ src/components/footer_components/FooterBase.tsx
@@ -1,8 +1,14 @@
+import AcceptedPayments from "./AcceptedPayments";
+import FooterLogo from "./FooterLogo";
+import FooterNav from "./FooterNav";
+
 
 const FooterBase = () => {
   return (
     <div className="max-w-1116 flex m-auto">
-      FooterBase
+      <FooterLogo />
+      <FooterNav />
+      <AcceptedPayments />
       
     </div>
   )
