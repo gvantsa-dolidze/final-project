fb23b0ff12654c81a8e1de42714996207d9ca086
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-25 14:43:20 +0400

    Refactor  FooterTop component layout with flex box for better alignment.

--- src/components/footer_components/FooterTop.tsx
+++ src/components/footer_components/FooterTop.tsx
@@ -2,8 +2,8 @@ import EmailInput from "./EmailInput"
 
 const FooterTop = () => {
   return (
-    <div>
-        <div>
+    <div className="flex justify-around">
+        <div className="text-left">
             <p>Join Our Newsletter</p>
             <p>We love to surprise our subscribers with occasional gifts.</p>
         </div>
