c8af79d8f86442c73f33e4b1317a622e8566472b
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-25 17:04:03 +0400

    Update FooterLogo component with branding and social media links.

--- src/components/footer_components/FooterLogo.tsx
+++ src/components/footer_components/FooterLogo.tsx
@@ -1,8 +1,21 @@
-
 const FooterLogo = () => {
   return (
-    <div>FooterLogo</div>
-  )
-}
+    <div className="space-y-4">
+      <div className="flex items-center space-x-2">
+        <img src="/img/logo/logomark_footer.png" alt="logo" />
+        <span>Ecommerce</span>
+      </div>
+      <div>
+        <p>DevCut is a YouTube channel for</p>
+        <p>practical project-based learning.</p>
+      </div>
+      <div className="flex gap-1">
+        <img src="/img/social icons/Github.png" alt="github" />
+        <img src="/img/social icons/Instagram.png" alt="instagram" />
+        <img src="/img/social icons/Youtube.png" alt="youtube" />
+      </div>
+    </div>
+  );
+};
 
-export default FooterLogo
+export default FooterLogo;
