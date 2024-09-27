84a605698447c7a9034a9e0d2a4210ebb5397250
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-27 22:35:53 +0400

    add images and styles in Banner component

--- src/components/homepage_components/Banner.tsx
+++ src/components/homepage_components/Banner.tsx
@@ -1,17 +1,25 @@
-import Button from "../elements/Button"
+import Button from "../elements/Button";
 const Banner = () => {
   return (
     <div className="bg-BackgroundGray py-40">
-      <div className="text-start max-w-1116 m-auto space-y-5">
-        <h2>Fresh Arrivals Online</h2>
-        <p>Discover Our Newest Collection Today.</p>
-        <Button 
-           label='View Collection'
-        />
-
+      <div className="text-start max-w-1116 m-auto space-y-5 flex justify-between">
+        <div className="space-y-5">
+          <h2>Fresh Arrivals Online</h2>
+          <p>Discover Our Newest Collection Today.</p>
+          <Button label="View Collection" />
+        </div>
+        <div>
+          <img src="/img/banner/star.png" alt="star" />
+          <img src="/img/banner/ellipse.png" alt="ellipse" />
+          <img
+            src="/img/banner/guy_img.png"
+            alt="guy_img"
+            className="w-[255px] h-[382]"
+          />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
