eed9bc9cc3410a962100cb672d52103ef97e70b1
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-27 22:45:40 +0400

    Positioned third image on top of the second image using Tailwind CSS for proper overlay in the banner component

--- src/components/homepage_components/Banner.tsx
+++ src/components/homepage_components/Banner.tsx
@@ -1,20 +1,28 @@
 import Button from "../elements/Button";
 const Banner = () => {
   return (
-    <div className="bg-BackgroundGray py-40">
+    <div className="bg-BackgroundGray py-5">
       <div className="text-start max-w-1116 m-auto space-y-5 flex justify-between">
-        <div className="space-y-5">
+        <div className="space-y-5 my-auto">
           <h2>Fresh Arrivals Online</h2>
           <p>Discover Our Newest Collection Today.</p>
           <Button label="View Collection" />
         </div>
-        <div>
-          <img src="/img/banner/star.png" alt="star" />
-          <img src="/img/banner/ellipse.png" alt="ellipse" />
+        <div className="relative">
+          <img
+            src="/img/banner/star.png"
+            alt="star"
+            className="absolute top-0 left-0"
+          />
+          <img
+            src="/img/banner/ellipse.png"
+            alt="ellipse"
+            className="w-full h-auto"
+          />
           <img
             src="/img/banner/guy_img.png"
             alt="guy_img"
-            className="w-[255px] h-[382]"
+            className="w-[255px] h-[382px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
           />
         </div>
       </div>
