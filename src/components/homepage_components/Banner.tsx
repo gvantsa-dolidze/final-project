8ee96b10426e4b8819934f2f93f6b9d273e229e7
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-27 22:14:29 +0400

    add some elements in Banner component

--- src/components/homepage_components/Banner.tsx
+++ src/components/homepage_components/Banner.tsx
@@ -1,6 +1,16 @@
+import Button from "../elements/Button"
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className="bg-BackgroundGray py-40">
+      <div className="text-start max-w-1116 m-auto space-y-5">
+        <h2>Fresh Arrivals Online</h2>
+        <p>Discover Our Newest Collection Today.</p>
+        <Button 
+           label='View Collection'
+        />
+
+      </div>
+    </div>
   )
 }
 
