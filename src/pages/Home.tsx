1b0b29900363be74904e66942a7eb7aa06709857
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-27 21:53:12 +0400

    Import homepage components for structured layout: Banner, AboutShipping, BestSelling, SecondBanner, and FeaturedLatest

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
+import AboutShipping from "../components/homepage_components/AboutShipping"
+import Banner from "../components/homepage_components/Banner"
+import BestSelling from "../components/homepage_components/BestSelling"
+import FeaturedLatest from "../components/homepage_components/FeaturedLatest"
+import SecondBanner from "../components/homepage_components/SecondBanner"
+
 const Home = () => {
   return (
     <div>
-      homepage
+      <Banner />
+      <AboutShipping />
+      <BestSelling />
+      <SecondBanner />
+      <FeaturedLatest />
     </div>
   )
 }
