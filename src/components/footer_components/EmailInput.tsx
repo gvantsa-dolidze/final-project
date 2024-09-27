9fe4f7f6c6d41ffce359cc1cd4d134a9a8f9e3d1
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-27 21:39:48 +0400

    add EmailInput styles

--- src/components/footer_components/EmailInput.tsx
+++ src/components/footer_components/EmailInput.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import Button from "../elements/Button";
 const EmailInput = () => {
   return (
-    <form className="flex items-center gap- ">
+    <form className="flex items-center gap-3">
       <input
         type="email"
         placeholder="Your email address"
-        className="outline-none"
+        className="outline-none border py-2 px-3 rounded-md bg-[#F6F6F6] text-sm box-border"
       />
       <Button 
          label='Subscribe'
