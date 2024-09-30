55c1766880f75bffd88e1e8c50fe5fd11a6ef258
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-30 21:57:17 +0400

    add styles in BestSelling component

--- src/components/homepage_components/BestSelling.tsx
+++ src/components/homepage_components/BestSelling.tsx
@@ -2,7 +2,7 @@ import Card from "../elements/Card";
 
 const BestSelling = () => {
   return (
-    <div className="max-w-1116 m-auto">
+    <div className="max-w-1116 m-auto py-16 space-y-5">
       <div className="text-center">
         <p>SHOP NOW</p>
         <h2>Best Selling</h2>
