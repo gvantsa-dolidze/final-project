ed97181fa3126660634f913c63c95fe120d4e7b0
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-30 21:25:01 +0400

    add content in SecondBanner.tsx

--- src/components/homepage_components/SecondBanner.tsx
+++ src/components/homepage_components/SecondBanner.tsx
@@ -1,7 +1,22 @@
-
+import Button from "../elements/Button"
 const SecondBanner = () => {
   return (
-    <div>SecondBanner</div>
+    <div className="bg-BackgroundGray py-5">
+    <div className="text-start max-w-1116 m-auto space-y-5 flex justify-between">
+      <div className="space-y-5 my-auto">
+        <h2>Browse Our Fashion Paradise!</h2>
+        <p>Step into a world of style and explore our diverse collection of clothing categories.</p>
+        <Button label="Start Browsing" />
+      </div>
+      <div className="">
+        <img
+          src="/img/banner/category_Image.png"
+          alt="star"
+          className="w-56"
+        />
+      </div>
+    </div>
+  </div>
   )
 }
 
