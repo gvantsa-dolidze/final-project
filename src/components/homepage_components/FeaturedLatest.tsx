185104edfe12d6394382364aa11dcdfee00bb77a
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-30 21:53:51 +0400

    import Card into FeaturedLatest.tsx component

--- src/components/homepage_components/FeaturedLatest.tsx
+++ src/components/homepage_components/FeaturedLatest.tsx
@@ -1,7 +1,20 @@
+import Card from "../elements/Card";
+import Button from "../elements/Button";
 
 const FeaturedLatest = () => {
   return (
-    <div>FeaturedLatest</div>
+    <div className="max-w-1116 m-auto py-16">
+    <div className="text-center space-x-2">
+      <Button label='Featured' />
+      <Button label='Latest'/>
+    </div>
+    <div className="flex gap-5 py-5">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  </div>
   )
 }
 
