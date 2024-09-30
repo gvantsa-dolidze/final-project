c74cace2ab27c3af520f688a6ca801de14750647
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-09-30 22:38:33 +0400

    update Card component

--- src/components/elements/Card.tsx
+++ src/components/elements/Card.tsx
@@ -1,17 +1,29 @@
-import React from "react";
-
+import Button from "./Button";
 interface CardProps {
   variant?: "primary" | "secondary" | "danger" | "outline";
 }
 const Card: React.FC<CardProps> = ({ variant = "primary" }) => {
   const styles: Record<string, string> = {
-    primary: "w-[264px] h-[434px] bg-[gray]",
+    primary: "w-[264px] h-[434px] text-start space-y-5",
     secondary: "",
     danger: "",
     outline: "",
   };
 
-  return <div className={`${styles[variant]}`}></div>;
+  return (
+    <div className={`${styles[variant]}`}>
+      <img
+        src=""
+        alt="product"
+        className="w-[237px] h-[312px] bg-BackgroundGray"
+      />
+      <h2>text text text</h2>
+      <div className="flex items-center gap-3">
+        <Button label="IN STOCK" />
+        <p>$000</p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
