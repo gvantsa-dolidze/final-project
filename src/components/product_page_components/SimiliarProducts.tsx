7719de16be717e259cf6d0546bd021e50142e57f
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-01 14:57:38 +0400

    Create SimiliarProducts and import in ProductPage.tsx

---
+++ src/components/product_page_components/SimiliarProducts.tsx
@@ -0,0 +1,21 @@
+import Card from "../elements/Card";
+
+const SimiliarProducts = () => {
+  return (
+    <div className="flex flex-col space-y-10">
+      <div className="">
+        <h3>You might also like</h3>
+        <p>SIMILAR PRODUCTS</p>
+      </div>
+
+      <div className="flex justify-center">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+};
+
+export default SimiliarProducts;
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,13 +1,14 @@
 import ImgSwiper from "../components/elements/ImgSwiper";
 import DetailCustomizer from "../components/product_page_components/DetailCustomizer";
+import SimiliarProducts from "../components/product_page_components/SimiliarProducts";
 const ProductPage = () => {
   return (
-    <div className="max-w-1116 m-auto py-10">
+    <div className="max-w-1116 m-auto py-10 space-y-10">
       <div className="flex">
        <ImgSwiper />
        <DetailCustomizer />
       </div>
-      
+      <SimiliarProducts />
     </div>
   );
 };
