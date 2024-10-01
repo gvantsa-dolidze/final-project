50185c9c583e6dd13496713aba3e092df950738e
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-01 14:16:52 +0400

    add styles

--- src/components/product_page_components/DetailCustomizer.tsx
+++ src/components/product_page_components/DetailCustomizer.tsx
@@ -5,7 +5,7 @@ import SizePiker from "../elements/SizePiker";
 
 const DetailCustomizer = () => {
   return (
-    <div className="space-y-2">
+    <div className="space-y-10">
       <div className="flex justify-between">
         <h2>Raw Black T-Shirt Lineup</h2>
         <button>
@@ -19,19 +19,19 @@ const DetailCustomizer = () => {
         </NavLink>
         <Button label="IN STOCK" variant="secondary" />
       </div>
-      <div>$0000</div>
-      <div>
+      <div className="text-start">$0000</div>
+      <div className="flex flex-col items-start">
         <h2>Available Colors</h2>
         <ColorPiker />
       </div>
       <div>
-        <h2>SELECT SIZE</h2>
+        <h2 className="text-start">SELECT SIZE</h2>
         <SizePiker />
       </div>
       <div>
-        <h2>Quantity</h2>
+        <h2 className="text-start">Quantity</h2>
       </div>
-      <div className="flex">
+      <div className="flex justify-between items-center">
         <Button label="Add to cart" /> likebtn
       </div>
       <div>— Free shipping on orders $100+</div>
