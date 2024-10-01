6f0dc6c1cc1dc946a1d912c7862a92cd670a1306
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-01 13:11:46 +0400

    Add DetailCustomizer component for product selection features

---
+++ src/components/product_page_components/DetailCustomizer.tsx
@@ -0,0 +1,52 @@
+import { NavLink } from "react-router-dom";
+import Button from "../elements/Button";
+import SelectColorRadioBtn from "../elements/SelectColorRadioBtn";
+import SelectSizeRadioBtn from "../elements/SelectSizeRadioBtn";
+const DetailCustomizer = () => {
+  return (
+    <div className="space-y-2">
+      <div className="flex">
+        <h2>Raw Black T-Shirt Lineup</h2>
+        <button>
+          <img src="/img/icons/Share.png" alt="share" />
+        </button>
+      </div>
+      <div className="flex">
+        <NavLink to="" className="flex">
+          <img src="/img/icons/Star.png" alt="star" />
+          <span>4.2 — 54 Reviews</span>
+        </NavLink>
+        <Button label="IN STOCK" variant="secondary" />
+      </div>
+      <div>$0000</div>
+      <div>
+        <h2>Available Colors</h2>
+        <div className="space-x-2">
+          <SelectColorRadioBtn variant="blue" />
+          <SelectColorRadioBtn variant="green" />
+          <SelectColorRadioBtn variant="red" />
+          <SelectColorRadioBtn variant="yellow" />
+          <SelectColorRadioBtn variant="pink" />
+        </div>
+      </div>
+      <div>
+        <h2>SELECT SIZE</h2>
+        <div className="space-x-2 flex">
+          <SelectSizeRadioBtn variant="s" />
+          <SelectSizeRadioBtn variant="m" />
+          <SelectSizeRadioBtn variant="l" />
+          <SelectSizeRadioBtn variant="xl" />
+          <SelectSizeRadioBtn variant="xxl" />
+        </div>
+      </div>
+      <div>
+        <h2>Quantity</h2>
+      </div>
+      <div className="flex" >
+        <Button label='Add to cart'/> likebtn
+      </div>
+    </div>
+  );
+};
+
+export default DetailCustomizer;
--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,10 +1,11 @@
 import ImgSwiper from "../components/elements/ImgSwiper";
-
+import DetailCustomizer from "../components/product_page_components/DetailCustomizer";
 const ProductPage = () => {
   return (
-    <div className="max-w-1116">
-      <div className="py-5">
-       <ImgSwiper /> 
+    <div className="max-w-1116 m-auto">
+      <div className="py-5 flex max-w-[1092px]">
+       <ImgSwiper />
+       <DetailCustomizer />
       </div>
       
     </div>
