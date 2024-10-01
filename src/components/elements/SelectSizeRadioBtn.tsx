f25dfafa214bcffa97269aa67e77e5d71a40224c
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-01 11:17:01 +0400

    Add SelectSizeRadioBtn component with styled radio buttons for size selection.

--- src/components/elements/SelectSizeRadioBtn.tsx
+++ src/components/elements/SelectSizeRadioBtn.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
-const SelectSizeRadioBtn = () => {
-  return (
-    <div>SelectSizeRadioBtn</div>
-  )
+interface SelectSizeRadioBtnProps {
+  variant?: "s" | "m" | "l" | "xl" | "xxl";
 }
+const SelectSizeRadioBtn: React.FC<SelectSizeRadioBtnProps> = ({
+  variant = "s",
+}) => {
+  const styles: Record<string, string> = {
+    s: "appearance-none border w-6 h-6 rounded cursor-pointer checked:border-[black]",
+    m: "appearance-none border w-6 h-6 rounded cursor-pointer checked:border-[black]",
+    l: "appearance-none border w-6 h-6 rounded cursor-pointer checked:border-[black]",
+    xl: "appearance-none border w-6 h-6 rounded cursor-pointer checked:border-[black]",
+    xxl: "appearance-none border w-6 h-6 rounded cursor-pointer checked:border-[black]",
+  };
 
-export default SelectSizeRadioBtn
+  return (
+    <label
+      htmlFor={`colorPiker-${variant}`}
+      className="relative flex items-center justify-center"
+    >
+      <input
+        type="radio"
+        className={`${styles[variant]}`}
+        name="color_piker"
+        id={`colorPiker-${variant}`}
+        value={variant}
+      ></input>
+      <span className="absolute cursor-pointer text-sm">{variant}</span>
+    </label>
+  );
+};
+
+export default SelectSizeRadioBtn;
--- src/components/homepage_components/SecondBanner.tsx
+++ src/components/homepage_components/SecondBanner.tsx
@@ -1,23 +1,26 @@
-import Button from "../elements/Button"
+import Button from "../elements/Button";
 const SecondBanner = () => {
   return (
     <div className="bg-BackgroundGray py-5">
-    <div className="text-start max-w-1116 m-auto space-y-5 flex justify-between">
-      <div className="space-y-5 my-auto">
-        <h2>Browse Our Fashion Paradise!</h2>
-        <p>Step into a world of style and explore our diverse collection of clothing categories.</p>
-        <Button label="Start Browsing" />
-      </div>
-      <div className="">
-        <img
-          src="/img/banner/category_Image.png"
-          alt="star"
-          className="w-56"
-        />
+      <div className="text-start max-w-1116 m-auto space-y-5 flex justify-between">
+        <div className="space-y-5 my-auto">
+          <h2>Browse Our Fashion Paradise!</h2>
+          <p>
+            Step into a world of style and explore our diverse collection of
+            clothing categories.
+          </p>
+          <Button label="Start Browsing" />
+        </div>
+        <div className="">
+          <img
+            src="/img/banner/category_Image.png"
+            alt="star"
+            className="w-56"
+          />
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default SecondBanner
+export default SecondBanner;
