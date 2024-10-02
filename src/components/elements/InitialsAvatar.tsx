e5f8e2f89772a35629fad51ba4f2b975a45da347
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-02 18:35:16 +0400

    update InitialAvatar component

--- src/components/elements/InitialsAvatar.tsx
+++ src/components/elements/InitialsAvatar.tsx
@@ -1,7 +1,7 @@
 const InitialsAvatar = () => {
   return (
-    <div className="rounded-full bg-[#F0F1FF] w-12 h-12 relative">
-      <span className="absolute left-0 right-0 bottom-3">AB</span>
+    <div className="">
+      <span className="rounded-full bg-[#F0F1FF] p-3">AB</span>
     </div>
   );
 };
