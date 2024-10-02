1dd002eef589928f83000f34331a379797db5213
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-02 13:05:56 +0400

    update DetailsContent component

--- src/components/product_page_components/AboutProduct.tsx
+++ src/components/product_page_components/AboutProduct.tsx
@@ -1,15 +1,17 @@
-import React from 'react'
 import DetailsAndReviews from './DetailsAndReviews'
 import DetailsContent from './DetailsContent'
 import ReviewsContent from './ReviewsContent'
 
 const AboutProduct = () => {
   return (
-    <div className='max-w-1116 m-auto'>
-      <DetailsAndReviews />
-      
+    <div className='max-w-1116 m-auto py-28'>
+      <div className='flex items-center justify-between gap-20'>
+        <DetailsAndReviews />
+
       <DetailsContent />
-      <ReviewsContent />
+      {/* <ReviewsContent /> */}
+      </div>
+      
     </div>
   )
 }
--- src/components/product_page_components/DetailsContent.tsx
+++ src/components/product_page_components/DetailsContent.tsx
@@ -1,8 +1,26 @@
-
 const DetailsContent = () => {
   return (
-    <div>DetailsContent</div>
-  )
-}
+    <div className="text-start space-y-10">
+      <h2>Details</h2>
+      <div className="space-y-20">
+        <p>
+          Elevate your everyday style with our Men's Black T-Shirts, the
+          ultimate wardrobe essential for modern men. Crafted with meticulous
+          attention to detail and designed for comfort, these versatile black
+          tees are a must-have addition to your collection. The classic black
+          color never goes out of style. Whether you're dressing up for a
+          special occasion or keeping it casual, these black t-shirts are the
+          perfect choice, effortlessly complementing any outfit.
+        </p>
+        <ul>
+          <li>Premium Quality</li>
+          <li>Versatile Wardrobe Staple</li>
+          <li>Available in Various Sizes</li>
+          <li>Tailored Fit</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default DetailsContent
+export default DetailsContent;
