5c55e9ccd7878f511ce77d6995021b86e24ef3a3
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-02 20:39:31 +0400

    update CheckBtn component and Categories component

--- src/components/elements/CheckBtn.tsx
+++ src/components/elements/CheckBtn.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const CheckBtn = () => {
-  return (
-    <div>CheckBtn</div>
-  )
+interface CheckBtnProps {
+  id: string;
+  label: string;
+  variant?: "blue";
 }
+const CheckBtn: React.FC<CheckBtnProps> = ({ variant = "blue", label, id}) => {
+  const styles: Record<string, string> = {
+    blue: "w-4 h-4",
+  };
 
-export default CheckBtn
+  return (
+    <label htmlFor={id} className="flex gap-2 items-center">
+      <input
+        type="checkbox"
+        className={`${styles[variant]}`}
+        name="product-category"
+        id={id}
+      ></input>
+      <span>{label}</span>
+    </label>
+  );
+};
+
+export default CheckBtn;
--- src/components/listing_page_components/Categories.tsx
+++ src/components/listing_page_components/Categories.tsx
@@ -1,7 +1,16 @@
+import CheckBtn from "../elements/CheckBtn"
+
 const Categories = () => {
   return (
-    <div>
-        jjj
+    <div className="flex flex-col text-start gap-3">
+      <h3>Categories</h3>
+        <CheckBtn label='Perfume' id='perfume'/>
+        <CheckBtn label='Trousers' id='trousers'/>
+        <CheckBtn label='Shoe' id='shoe'/>
+        <CheckBtn label='Handbag' id='handbag' />
+        <CheckBtn label='Hat' id='hat'/>
+        <CheckBtn label='Thermos' id='thermos'/>
+
     </div>
   )
 }
