3edc7295e933d1fd86ca658cd0c897bca851c2a3
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-02 15:11:16 +0400

    Create CommentSection component and import in ReviewsContent.tsx

--- src/components/product_page_components/ReviewsContent.tsx
+++ src/components/product_page_components/ReviewsContent.tsx
@@ -1,8 +1,10 @@
 import ReviewAndSort from "./ReviewAndSort";
+import CommentSection from "./CommentSection";
 const ReviewsContent = () => {
   return (
     <div className="w-[727px]">
       <ReviewAndSort />
+      <CommentSection />
     </div>
   );
 };
