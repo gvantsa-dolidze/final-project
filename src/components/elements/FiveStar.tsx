98b8f52c9e397fa56217b4de6c2074e07a8447a5
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-02 16:34:37 +0400

    Create FiveStar component

---
+++ src/components/elements/FiveStar.tsx
@@ -0,0 +1,14 @@
+
+const FiveStar = () => {
+  return (
+    <div className="flex gap-2">
+        <img src="/img/icons/Empty Star.png" alt="star" />
+        <img src="/img/icons/Empty Star.png" alt="star" />
+        <img src="/img/icons/Empty Star.png" alt="star" />
+        <img src="/img/icons/Empty Star.png" alt="star" />
+        <img src="/img/icons/Empty Star.png" alt="star" />
+    </div>
+  )
+}
+
+export default FiveStar
