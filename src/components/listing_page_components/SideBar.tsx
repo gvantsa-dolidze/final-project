d8c58ceccc2571667bfd5c504fca4f85b414d25f
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-02 20:49:18 +0400

    import ColorPiker and SizePiker component in SideBar

--- src/components/listing_page_components/SideBar.tsx
+++ src/components/listing_page_components/SideBar.tsx
@@ -1,11 +1,21 @@
-import Categories from "./Categories"
+import ColorPiker from "../elements/ColorPiker";
+import SizePiker from "../elements/SizePiker";
+import Categories from "./Categories";
 
 const SideBar = () => {
   return (
-    <div className="border p-2">
-        <Categories />
+    <div className="border p-2 space-y-5">
+      <Categories />
+      <div className="space-y-3 text-start">
+        <h3>Color</h3>
+        <ColorPiker />
+      </div>
+      <div className="space-y-3 text-start">
+      <h3>Size</h3>
+        <SizePiker />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
