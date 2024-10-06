6233b7ce3457a7a63ab6b499d2def2dc2c83fd76
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-06 22:45:22 +0400

    change button style

--- src/components/elements/Button.tsx
+++ src/components/elements/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 }
 const Button: React.FC<ButtonProps> = ({ label, variant = "primary", destination }) => {
   const styles: Record<string, string> = {
-    primary: "bg-MidnightBlue text-white px-4 py-2 rounded-md text-sm",
+    primary: "bg-MidnightBlue text-white px-8 py-3 rounded-md text-sm",
     secondary: "border rounded-full px-4 py-1 text-sm hover:bg-BackgroundGray",
     danger: "",
     outline: "",
