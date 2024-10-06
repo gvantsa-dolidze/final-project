4e24e7060888191bf8477fc7f117bc3c5090458b
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-06 22:46:48 +0400

    Add hover to SelectSizeRadioBtn component

--- src/components/elements/SelectSizeRadioBtn.tsx
+++ src/components/elements/SelectSizeRadioBtn.tsx
@@ -5,11 +5,11 @@ const SelectSizeRadioBtn: React.FC<SelectSizeRadioBtnProps> = ({
   variant = "s",
 }) => {
   const styles: Record<string, string> = {
-    s: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black]",
-    m: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black]",
-    l: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black]",
-    xl: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black]",
-    xxl: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black]",
+    s: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black] hover:bg-BackgroundGray",
+    m: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black] hover:bg-BackgroundGray",
+    l: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black] hover:bg-BackgroundGray",
+    xl: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black] hover:bg-BackgroundGray",
+    xxl: "appearance-none border w-10 h-10 rounded cursor-pointer checked:border-[black] hover:bg-BackgroundGray",
   };
 
   return (
