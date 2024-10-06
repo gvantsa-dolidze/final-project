1981b1cc59a83ba17d2b67f8c6313da12650d721
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-06 19:19:02 +0400

    Import Breadcrumb component in ListingPage

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -2,9 +2,12 @@ import SideBar from "../components/listing_page_components/SideBar";
 import GridContainer from "../components/listing_page_components/GridContainer";
 import AppliedFilters from "../components/listing_page_components/AppliedFilters";
 import ShowingResults from "../components/listing_page_components/ShowingResults";
+import Breadcrumb from "../components/elements/Breadcrumb";
 const ListingPage = () => {
   return (
-    <div className="max-w-1116 m-auto gap-10 flex py-10">
+    <div>
+      <Breadcrumb />
+      <div className="max-w-1116 m-auto gap-10 flex py-10">
       <SideBar />
       <div className="flex flex-col gap-10">
         <div className="text-start space-y-10">
@@ -14,6 +17,8 @@ const ListingPage = () => {
         <GridContainer />
       </div>
     </div>
+    </div>
+    
   );
 };
 
