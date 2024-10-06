99114bab325863ebd5d60a277b8f85bb24130c7a
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-06 19:16:02 +0400

    Update Breadcrumb component

--- src/components/elements/Breadcrumb.tsx
+++ src/components/elements/Breadcrumb.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Breadcrumb = () => {
   return (
-    <div>Breadcrumb</div>
-  )
-}
+    <div className="max-w-1116 m-auto py-5">
+      <nav className="flex" aria-label="Breadcrumb">
+        <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+          <li className="inline-flex items-center">
+            <NavLink
+              to=""
+              className="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white"
+            >
+              Ecommerce
+            </NavLink>
+          </li>
+          <li>
+            <div className="flex items-center">
+              <svg
+                className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <NavLink
+                to=""
+                className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white"
+              >
+                Search
+              </NavLink>
+            </div>
+          </li>
+          <li aria-current="page">
+            <div className="flex items-center">
+              <svg
+                className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 6 10"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="m1 9 4-4-4-4"
+                />
+              </svg>
+              <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400">
+                Product
+              </span>
+            </div>
+          </li>
+        </ol>
+      </nav>
+    </div>
+  );
+};
 
-export default Breadcrumb
+export default Breadcrumb;
