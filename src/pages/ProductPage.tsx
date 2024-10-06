95f1d5afdd244c224dc2f21575639efe8abaca37
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-06 19:20:34 +0400

    import Breadcrumb in ProductPage

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -2,15 +2,19 @@ import ImgSwiper from "../components/elements/ImgSwiper";
 import DetailCustomizer from "../components/product_page_components/DetailCustomizer";
 import SimiliarProducts from "../components/product_page_components/SimiliarProducts";
 import AboutProduct from "../components/product_page_components/AboutProduct";
+import Breadcrumb from "../components/elements/Breadcrumb";
 const ProductPage = () => {
   return (
-    <div className="max-w-1116 m-auto py-10 space-y-10">
-      <div className="flex">
-       <ImgSwiper />
-       <DetailCustomizer />
+    <div>
+      <Breadcrumb />
+      <div className="max-w-1116 m-auto py-10 space-y-10">
+        <div className="flex">
+          <ImgSwiper />
+          <DetailCustomizer />
+        </div>
+        <AboutProduct />
+        <SimiliarProducts />
       </div>
-<AboutProduct />
-      <SimiliarProducts />
     </div>
   );
 };
