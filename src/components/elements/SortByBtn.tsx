4c8737f8888265ae63c75d8fb9ba22152fe715b7
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-06 20:51:17 +0400

    add Dropdown functionality in SortByBtn component

--- src/components/elements/SortByBtn.tsx
+++ src/components/elements/SortByBtn.tsx
@@ -1,9 +1,61 @@
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import { NavLink } from "react-router-dom";
 const SortByBtn = () => {
   return (
-    <button className="flex">
-      <span>SORT BY</span>
-      <img src="/img/icons/Chevron Down.png" alt="arrow_down" />
-    </button>
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2  hover:bg-gray-50">
+          SORT BY
+          <ChevronDownIcon
+            aria-hidden="true"
+            className="-mr-1 h-5 w-5 text-gray-400"
+          />
+        </MenuButton>
+      </div>
+
+      <MenuItems
+        transition
+        className="absolute right-0 z-10 mt-2 w-44 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+      >
+        <div className="py-1">
+          <MenuItem>
+            <NavLink
+              to=""
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Most Popular
+            </NavLink>
+          </MenuItem>
+          <MenuItem>
+            <NavLink
+              to=""
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Least Popular
+            </NavLink>
+          </MenuItem>
+          <MenuItem>
+            <NavLink
+              to=""
+              className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+            >
+              Newest First
+            </NavLink>
+          </MenuItem>
+          <form action="#" method="POST">
+            <MenuItem>
+              <button
+                type="submit"
+                className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
+              >
+                Oldest First
+              </button>
+            </MenuItem>
+          </form>
+        </div>
+      </MenuItems>
+    </Menu>
   );
 };
 
