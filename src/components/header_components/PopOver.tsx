76e86fbd30fc5146006cd21c76aa800f0ab4e4f6
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-06 21:53:37 +0400

    changes

--- src/components/header_components/PopOver.tsx
+++ src/components/header_components/PopOver.tsx
@@ -27,12 +27,12 @@ const PopOver: React.FC<PopOverProps> = ({
           <img src="/img/icons/Chevron Down.png" alt="arrow_down" />
         </PopoverButton>
         <PopoverPanel anchor="bottom" className="flex flex-col gap-3 bg-white rounded-md border border-gray-200 mt-3">
-          <NavLink to="/analytics" className="hover:bg-BackgroundGray px-4 py-2">{perfume}</NavLink>
-          <NavLink to="/engagement" className="hover:bg-BackgroundGray px-4 py-2">{trousers}</NavLink>
-          <NavLink to="/security" className="hover:bg-BackgroundGray px-4 py-2">{shoe}</NavLink>
-          <NavLink to="/integrations" className="hover:bg-BackgroundGray px-4 py-2">{handbag}</NavLink>
-          <NavLink to="/integrations" className="hover:bg-BackgroundGray px-4 py-2">{hat}</NavLink>
-          <NavLink to="/integrations" className="hover:bg-BackgroundGray px-4 py-2">{thermos}</NavLink>
+          <NavLink to="/perfume" className="hover:bg-BackgroundGray px-4 py-2">{perfume}</NavLink>
+          <NavLink to="/trousers" className="hover:bg-BackgroundGray px-4 py-2">{trousers}</NavLink>
+          <NavLink to="/shoe" className="hover:bg-BackgroundGray px-4 py-2">{shoe}</NavLink>
+          <NavLink to="/handbag" className="hover:bg-BackgroundGray px-4 py-2">{handbag}</NavLink>
+          <NavLink to="/hat" className="hover:bg-BackgroundGray px-4 py-2">{hat}</NavLink>
+          <NavLink to="/thermos" className="hover:bg-BackgroundGray px-4 py-2">{thermos}</NavLink>
         </PopoverPanel>
       </Popover>
   )
