a866b9b4a1e7feff58c5aaf30fb3f29d5650fbec
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-07 22:49:15 +0400

    Update CartElement and import in CartPage component

--- src/components/cart_page_components/CartElement.tsx
+++ src/components/cart_page_components/CartElement.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
 
 const CartElement = () => {
   return (
-    <div>CartElement</div>
+    <div>
+          <h3 className="border-b pb-5 text-start">Your cart</h3>
+          <div className="flex items-center justify-between gap-5 border rounded-md mt-5">
+            <img src="/img/cover.png" alt="product" className="w-20" />
+            <div className="text-start">
+              <h3>Raw Black T-Shirt Lineup</h3>
+              <div className="flex gap-2 items-center">
+                color: <div className="bg-green-700 w-3 h-3 rounded-full"></div>{" "}
+                - Size: M
+              </div>
+            </div>
+            <div>$75.00</div>
+            <div> - 1 + </div>
+            <button>
+              <img src="/img/icons/x.png" alt="x" />
+            </button>
+          </div>
+        </div>
   )
 }
 
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,3 +1,4 @@
+import CartElement from "../components/cart_page_components/CartElement";
 import Breadcrumb from "../components/elements/Breadcrumb";
 import Button from "../components/elements/Button";
 
@@ -8,25 +9,8 @@ const CartPage = () => {
         <Breadcrumb />
       </div>
       <div className="flex justify-between m-auto max-w-1116 py-10">
-        {/* Cart element */}
-        <div>
-          <h3 className="border-b pb-5 text-start">Your cart</h3>
-          <div className="flex items-center justify-between gap-5 border rounded-md mt-5">
-            <img src="/img/cover.png" alt="product" className="w-20" />
-            <div className="text-start">
-              <h3>Raw Black T-Shirt Lineup</h3>
-              <div className="flex gap-2 items-center">
-                color: <div className="bg-green-700 w-3 h-3 rounded-full"></div>{" "}
-                - Size: M
-              </div>
-            </div>
-            <div>$75.00</div>
-            <div> - 1 + </div>
-            <button>
-              <img src="/img/icons/x.png" alt="x" />
-            </button>
-          </div>
-        </div>
+        
+        <CartElement />
 
         {/* Order Summary */}
         <div className="border rounded-md py-10 px-2  flex flex-col w-96 gap-5">
