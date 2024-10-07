12f62bd64dff92d26e746650f9ae190d357d17fc
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-07 22:38:39 +0400

    changes

--- src/components/header_components/LoginAndCart.tsx
+++ src/components/header_components/LoginAndCart.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 const LoginAndCart = () => {
   return (
     <div className="flex gap-8 shrink-0 items-center">
-      <NavLink to="//cart_page">
+      <NavLink to="/cart_page">
         <img src="/img/icons/cart.png" alt="login"/>
       </NavLink>
 
