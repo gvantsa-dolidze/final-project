148f6614bae84970688f09f833a16d9a8e649800
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 00:37:03 +0400

    create SuccessfulOrder component and import in AfterPaymentPage also import Breadcrumb

---
+++ src/components/after_payment_page/SuccessfulOrder.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuccessfulOrder = () => {
+  return (
+    <div>SuccessfulOrder</div>
+  )
+}
+
+export default SuccessfulOrder
--- src/pages/AfterPaymentPage.tsx
+++ src/pages/AfterPaymentPage.tsx
@@ -1,7 +1,17 @@
+import SuccessfulOrder from "../components/after_payment_page/SuccessfulOrder";
+import Breadcrumb from "../components/elements/Breadcrumb";
+
 const AfterPaymentPage = () => {
   return (
-    <div>AfterPaymentPage</div>
-  )
-}
+    <div>
+      <div className="bg-[#D5E5D7] py-5">
+        <Breadcrumb />
+      </div>
+      <div className="max-w-1116 m-auto">
+        <SuccessfulOrder />
+      </div>
+    </div>
+  );
+};
 
-export default AfterPaymentPage
+export default AfterPaymentPage;
