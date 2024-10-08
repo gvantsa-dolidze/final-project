5c5760cb29868e94f33dbc1fb1fcc01f8dd5314f
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-08 20:02:37 +0400

    import InputElement component in EmailInput and delete hardcode

--- src/components/footer_components/EmailInput.tsx
+++ src/components/footer_components/EmailInput.tsx
@@ -1,17 +1,11 @@
-import React from "react";
 import Button from "../elements/Button";
+import InputElement from "../elements/InputElement";
+
 const EmailInput = () => {
   return (
     <form className="flex items-center gap-3">
-      <input
-        type="email"
-        placeholder="Your email address"
-        className="outline-none border py-2 px-3 rounded-md bg-[#F6F6F6] text-sm box-border"
-      />
-      <Button 
-         label='Subscribe'
-         destination='/' 
-      />
+      <InputElement text="" type="email" placeholder="Your email address" />
+      <Button label="Subscribe" destination="/" />
     </form>
   );
 };
