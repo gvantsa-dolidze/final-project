650c027824baca00b3d03cab819131af27766bea
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-08 23:25:45 +0400

    Create ShippingAddress component and import in CheckoutPage

--- src/pages/CheckoutPage.tsx
+++ src/pages/CheckoutPage.tsx
@@ -1,5 +1,6 @@
+import ShippingAddress from "../components/checkout_page_components/ShippingAddress";
 import Breadcrumb from "../components/elements/Breadcrumb";
-import InputElement from "../components/elements/InputElement";
+// import InputElement from "../components/elements/InputElement";
 
 const CheckoutPage = () => {
   return (
@@ -8,22 +9,7 @@ const CheckoutPage = () => {
         <Breadcrumb />
       </div>
       <div className="flex justify-between py-10 max-w-1116 m-auto">
-        <div>
-          <h3 className="text-start">Shipping Address</h3>
-          <form className="space-y-5 mt-5">
-            <InputElement text="Street Address" type="text" placeholder="" />
-            <div className="grid gap-x-6 gap-y-10 md:grid-cols-1 lg:grid-cols-2 xl:gap-x-8">
-              <InputElement text="City" type="text" placeholder=""/>
-              <InputElement text="State" type="text" placeholder=""/>
-
-              <InputElement text="Zip Code" type="number" placeholder=""/>
-              <InputElement text="Country" type="text" placeholder=""/>
-
-              <InputElement text="Email" type="email" placeholder=""/>
-              <InputElement text="Full name"  type="text" placeholder=""/>
-            </div>
-          </form>
-        </div>
+        <ShippingAddress />
 
         <div></div>
       </div>
