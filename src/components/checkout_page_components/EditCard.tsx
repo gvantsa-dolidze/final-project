693eef969f2b0a1e687e87920d5767f86ef33210
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 00:21:37 +0400

    Create EditCard component and import in YourOrder component

---
+++ src/components/checkout_page_components/EditCard.tsx
@@ -0,0 +1,18 @@
+import Button from "../elements/Button"
+import FullRoundedImg from "../elements/FullRoundedImg"
+
+const EditCard = () => {
+  return (
+    <div className="flex  items-center gap-5 mt-5">
+        <div className="flex gap-5">
+          <FullRoundedImg />
+          <FullRoundedImg />
+          <FullRoundedImg />
+        </div>
+
+        <Button label="Edit Card" destination="/cart_page" />
+      </div>
+  )
+}
+
+export default EditCard
--- src/components/checkout_page_components/YourOrder.tsx
+++ src/components/checkout_page_components/YourOrder.tsx
@@ -1,21 +1,11 @@
-import Button from "../elements/Button";
-import FullRoundedImg from "../elements/FullRoundedImg";
+import EditCard from "./EditCard";
 import PlaceOrder from "./PlaceOrder";
 
 const YourOrder = () => {
   return (
     <div className="border-l-2 pl-20">
       <h3 className="text-start">Your Order</h3>
-      <div className="flex  items-center gap-5 mt-5">
-        <div className="flex gap-5">
-          <FullRoundedImg />
-          <FullRoundedImg />
-          <FullRoundedImg />
-        </div>
-
-        <Button label="Edit Card" destination="/cart_page" />
-      </div>
-
+      <EditCard />
       <PlaceOrder />
     </div>
   );
