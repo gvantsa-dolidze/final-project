975065d9aafeb22e6a1384ba13fddf21e13f312e
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 00:17:22 +0400

    Create  FullRoundedImg component and import in YourOrder component

--- src/components/checkout_page_components/YourOrder.tsx
+++ src/components/checkout_page_components/YourOrder.tsx
@@ -1,36 +1,24 @@
-import Button from "../elements/Button"
-import PlaceOrder from "./PlaceOrder"
+import Button from "../elements/Button";
+import FullRoundedImg from "../elements/FullRoundedImg";
+import PlaceOrder from "./PlaceOrder";
+
 const YourOrder = () => {
   return (
     <div className="border-l-2 pl-20">
-          <h3 className="text-start">Your Order</h3>
-          <div className="flex  items-center gap-5 mt-5">
-            <div className="flex gap-5">
-              <img
-                src="/img/cover.png"
-                alt="product"
-                className="w-16 h-16 p-3 bg-BackgroundGray rounded-full"
-              />
-
-              <img
-                src="/img/cover1.png"
-                alt="product"
-                className="w-16 h-16 p-3 bg-BackgroundGray rounded-full"
-              />
-
-              <img
-                src="/img/cover2.png"
-                alt="product"
-                className="w-16 h-16 p-3 bg-BackgroundGray rounded-full"
-              />
-            </div>
-
-            <Button label="Edit Card" destination="/cart_page" />
-          </div>
-          
-          <PlaceOrder />
+      <h3 className="text-start">Your Order</h3>
+      <div className="flex  items-center gap-5 mt-5">
+        <div className="flex gap-5">
+          <FullRoundedImg />
+          <FullRoundedImg />
+          <FullRoundedImg />
         </div>
-  )
-}
 
-export default YourOrder
+        <Button label="Edit Card" destination="/cart_page" />
+      </div>
+
+      <PlaceOrder />
+    </div>
+  );
+};
+
+export default YourOrder;
---
+++ src/components/elements/FullRoundedImg.tsx
@@ -0,0 +1,12 @@
+
+const FullRoundedImg = () => {
+  return (
+    <img
+    src="/img/cover.png"
+    alt="product"
+    className="w-16 h-16 p-3 bg-BackgroundGray rounded-full"
+  />
+  )
+}
+
+export default FullRoundedImg
