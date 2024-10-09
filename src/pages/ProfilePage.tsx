b7f4467ecaa520e65d4903da43554984699bcaab
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 14:52:57 +0400

    changes

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,23 +1,21 @@
 import { Outlet } from "react-router-dom";
 import ProfileSideBar from "../components/profile_pages_components/ProfileSideBar";
 import Breadcrumb from "../components/elements/Breadcrumb";
-import Button from "../components/elements/Button";
 
 const ProfilePage = () => {
   return (
     <div>
-        <div className="bg-BackgroundGray py-5">
-            <Breadcrumb />
+      <div className="bg-BackgroundGray py-5">
+        <Breadcrumb />
+      </div>
+      <div className="max-w-1116 m-auto flex py-10">
+        <ProfileSideBar />
+        {/* Content Area */}
+        <div className="flex-1">
+          <Outlet />
         </div>
-        <div className="max-w-1116 m-auto flex py-10">
-      <ProfileSideBar />
-      {/* Content Area */}
-      <div className="flex-1">
-        <Outlet />
       </div>
     </div>
-    </div>
-    
   );
 };
 
