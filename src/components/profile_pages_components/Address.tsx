4fde6149ceb48f94947bd0761697bf627fd66d6e
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 15:13:40 +0400

    update Address component

--- src/components/profile_pages_components/Address.tsx
+++ src/components/profile_pages_components/Address.tsx
@@ -1,7 +1,19 @@
+import ShippingAddress from "../checkout_page_components/ShippingAddress"
+import Button from "../elements/Button"
 
 const Address = () => {
   return (
-    <div>Address</div>
+    <div className="flex justify-center">
+      <div className="w-96">
+        <ShippingAddress />
+        <div className="flex mt-5">
+         <Button label='Save Changes' destination='/'/>   
+        </div>
+        
+      </div>
+        
+        
+    </div>
   )
 }
 
