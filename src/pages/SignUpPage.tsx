6a5b3f8de759487e5c184cdaae937ba69241b51f
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 11:18:21 +0400

    import ContinueWithGoogle component in SignUpPage

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -1,6 +1,8 @@
 import Breadcrumb from "../components/elements/Breadcrumb";
 import Button from "../components/elements/Button";
 import InputElement from "../components/elements/InputElement";
+import ContinueWithGoogle from "../components/authentication_pages_components/ContinueWithGoogle";
+
 const SignUpPage = () => {
   return (
     <div>
@@ -10,13 +12,14 @@ const SignUpPage = () => {
       <div className="max-w-1116 m-auto flex justify-center">
         <div className="py-10">
           <div className="flex flex-col items-center gap-5 w-96 py-10">
-            <div className="flex flex-col gap-5 w-full">
+            {/* <div className="flex flex-col gap-5 w-full">
               <Button
                 label="Continue with Google"
                 destination="/"
                 variant="light"
               />
-            </div>
+            </div> */}
+            <ContinueWithGoogle />
 
             <div className="flex items-center w-full justify-between">
               <hr className="w-44" /> <p>OR</p> <hr className="w-44" />
