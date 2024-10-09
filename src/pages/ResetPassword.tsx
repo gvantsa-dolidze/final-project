fcbd6a7e342a1229f3219f71df645cf0663f97cd
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 11:10:43 +0400

    change input element's type in ResetPassword

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,24 +1,24 @@
-import Breadcrumb from "../components/elements/Breadcrumb"
+import Breadcrumb from "../components/elements/Breadcrumb";
 import Button from "../components/elements/Button";
 import InputElement from "../components/elements/InputElement";
 
 const ResetPassword = () => {
   return (
     <div>
-    <div className="bg-BackgroundGray">
-      <Breadcrumb />
+      <div className="bg-BackgroundGray">
+        <Breadcrumb />
+      </div>
+      <div className="max-w-1116 m-auto flex justify-center">
+        <form action="" className="space-y-5 py-32 w-96">
+          <InputElement text="New password" type="password" placeholder="" />
+          <InputElement text="Confirm password" type="password" placeholder="" />
+          <div className="flex flex-col gap-5">
+            <Button label="Reset password" destination="/" />
+          </div>
+        </form>
+      </div>
     </div>
-    <div className="max-w-1116 m-auto flex justify-center">
-    <form action="" className="space-y-5 py-32 w-96">
-            <InputElement text="Email" type="email" placeholder="" />
-            <InputElement text="password" type="password" placeholder="" />
-            <div className="flex flex-col gap-5">
-              <Button label="Reset password" destination="/" />
-            </div>
-          </form>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default ResetPassword
+export default ResetPassword;
