db345925f7fcb78740005cd709da4a1e428e0a27
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 14:42:31 +0400

    import   Breadcrumb component in ProfilePage

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,15 +1,22 @@
 import { Outlet } from "react-router-dom";
 import ProfileSideBar from "../components/profile_pages_components/ProfileSideBar";
+import Breadcrumb from "../components/elements/Breadcrumb";
 
 const ProfilePage = () => {
   return (
-    <div className="max-w-1116 m-auto flex">
+    <div>
+        <div className="bg-BackgroundGray py-5">
+            <Breadcrumb />
+        </div>
+        <div className="max-w-1116 m-auto flex py-10">
       <ProfileSideBar />
       {/* Content Area */}
       <div className="flex-1">
         <Outlet />
       </div>
     </div>
+    </div>
+    
   );
 };
 
