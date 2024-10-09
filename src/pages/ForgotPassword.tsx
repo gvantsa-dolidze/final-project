0b8915c50acc5ac3050b8cd7d9b9751b7dc1b97a
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 11:54:04 +0400

    change styles

--- src/pages/ForgotPassword.tsx
+++ src/pages/ForgotPassword.tsx
@@ -1,7 +1,6 @@
 import ForgotPasswordForm from "../components/authentication_pages_components/ForgotPasswordForm";
 import Breadcrumb from "../components/elements/Breadcrumb";
 
-
 const ForgotPassword = () => {
   return (
     <div>
@@ -9,17 +8,15 @@ const ForgotPassword = () => {
         <Breadcrumb />
       </div>
       <div className="max-w-1116 m-auto flex justify-center">
-        <div className="w-96 py-32">
-          <div className="flex flex-col items-center gap-5 ">
-            <div>
-              <p>
-                Please enter the email address associated with your account.
-                We'll promptly send you a link to reset your password.
-              </p>
-            </div>
-            
-            <ForgotPasswordForm />
+        <div className="w-96 py-32 space-y-10">
+          <div>
+            <p>
+              Please enter the email address associated with your account. We'll
+              promptly send you a link to reset your password.
+            </p>
           </div>
+
+          <ForgotPasswordForm />
         </div>
       </div>
     </div>
