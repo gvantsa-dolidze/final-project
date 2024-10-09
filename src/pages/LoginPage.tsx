41f53c1a4a51c27a34aec55e865e53c6cad24afb
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 08:50:00 +0400

    Rename Authentication page to LoginPage

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Breadcrumb from "../components/elements/Breadcrumb";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <div className="bg-BackgroundGray">
+        <Breadcrumb />
+      </div>
+      <div className="max-w-1116 m-auto">
+        form
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
