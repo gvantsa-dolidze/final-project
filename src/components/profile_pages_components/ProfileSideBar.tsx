424533d43e2975f34e3021d8fe8af08f792c2974
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 14:40:38 +0400

    add styles for ProfileSideBar's NavLink Element

--- src/components/profile_pages_components/ProfileSideBar.tsx
+++ src/components/profile_pages_components/ProfileSideBar.tsx
@@ -2,24 +2,24 @@ import { NavLink } from "react-router-dom"
 
 const ProfileSideBar = () => {
   return (
-    <div className="flex flex-col w-1/4 border-r-2">
-        <NavLink to="orders" className="mb-2">
-          Orders
+    <div className="flex flex-col w-1/4 border-r-2 p-5">
+        <NavLink to="orders" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
+          <img src="/img/icons/Add to cart.png" alt="img" /> <p>Orders</p>
         </NavLink>
-        <NavLink to="wishlists" className="mb-2">
-          Wishlists
+        <NavLink to="wishlists" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
+          <img src="/img/icons/Heart.png" alt="img" /> <p>Wishlists</p>
         </NavLink>
-        <NavLink to="address" className="mb-2">
-          Address
+        <NavLink to="address" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
+          <img src="/img/icons/Delivery.png" alt="img" /> <p>Address</p>
         </NavLink>
-        <NavLink to="password" className="mb-2">
-          Password
+        <NavLink to="password" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
+          <img src="/img/icons/Key.png" alt="img" /> <p>Password</p>
         </NavLink>
-        <NavLink to="account_detail" className="mb-2">
-          Account Detail
+        <NavLink to="account_detail" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
+          <img src="/img/icons/User.png" alt="img" /> <p>Account Detail</p>
         </NavLink>
-        <NavLink to="logout" className="mb-2">
-          Logout
+        <NavLink to="logout" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
+          <img src="/img/icons/Logout.png" alt="img" /> <p>Logout</p>
         </NavLink>
       </div>
   )
