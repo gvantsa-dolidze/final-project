06d15ca64e73f24635ea8da7e5ccec42c0ed61ec
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 11:57:42 +0400

    create ResetPasswordForm and import in ResetPassword page

---
+++ src/components/authentication_pages_components/ResetPasswordForm.tsx
@@ -0,0 +1,15 @@
+import Button from "../elements/Button";
+import InputElement from "../elements/InputElement";
+const ResetPasswordForm = () => {
+  return (
+    <form action="" className="space-y-5 py-32 w-96">
+      <InputElement text="New password" type="password" placeholder="" />
+      <InputElement text="Confirm password" type="password" placeholder="" />
+      <div className="flex flex-col gap-5">
+        <Button label="Reset password" destination="/" />
+      </div>
+    </form>
+  );
+};
+
+export default ResetPasswordForm;
--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,6 +1,5 @@
+import ResetPasswordForm from "../components/authentication_pages_components/ResetPasswordForm";
 import Breadcrumb from "../components/elements/Breadcrumb";
-import Button from "../components/elements/Button";
-import InputElement from "../components/elements/InputElement";
 
 const ResetPassword = () => {
   return (
@@ -9,13 +8,7 @@ const ResetPassword = () => {
         <Breadcrumb />
       </div>
       <div className="max-w-1116 m-auto flex justify-center">
-        <form action="" className="space-y-5 py-32 w-96">
-          <InputElement text="New password" type="password" placeholder="" />
-          <InputElement text="Confirm password" type="password" placeholder="" />
-          <div className="flex flex-col gap-5">
-            <Button label="Reset password" destination="/" />
-          </div>
-        </form>
+        <ResetPasswordForm />
       </div>
     </div>
   );
