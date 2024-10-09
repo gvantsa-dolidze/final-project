2bc29c72b9bae5e9b25a365b2f92fa8a64f1fcb9
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-09 16:32:35 +0400

    Update Password component

--- src/components/profile_pages_components/Password.tsx
+++ src/components/profile_pages_components/Password.tsx
@@ -1,7 +1,20 @@
+import InputElement from "../elements/InputElement";
+import Button from "../elements/Button";
 
 const Password = () => {
   return (
-    <div>Password</div>
+    <div className="flex justify-start pl-10">
+      <div className="w-96 space-y-5 text-start">
+        <h3>Change Password</h3>
+        <form action="" className="space-y-5">
+          <InputElement text="New Password" type="password" placeholder="" />
+          <InputElement text="Confirm Password" type="password" placeholder="" />
+          <div>
+            <Button label="Change Password" destination="/" />
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
