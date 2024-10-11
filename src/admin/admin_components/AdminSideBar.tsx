dd2ab5c60c1b5ec700722d7226b9dd4477e0a56d
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-12 01:00:33 +0400

    Create AdminLogo and import In AdminSideBar component

--- src/admin/admin_components/AdminSideBar.tsx
+++ src/admin/admin_components/AdminSideBar.tsx
@@ -1,15 +1,19 @@
 import { NavLink } from "react-router-dom"
-
+import AdminLogo from "../admin_elements/AdminLogo"
 const AdminSideBar = () => {
   return (
-    <div className="flex flex-col w-1/4">
+    <div>
+<AdminLogo />
+       <div className="flex flex-col w-1/4">
         <NavLink to='dashboard'>Dashboard</NavLink>
         <NavLink to='products'>Products</NavLink>
         <NavLink to='admins_orders'>Orders</NavLink>
         <NavLink to='customers'>Customers</NavLink>
         <NavLink to='reviews'>reviews</NavLink>
         <NavLink to='settings'>settings</NavLink>
+    </div> 
     </div>
+    
   )
 }
 
