c066827045051b73b6e09895cf89d4781399ca9e
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-12 17:30:09 +0400

    Create Component BannerImg and import in Banner component

---
+++ src/components/elements/BannerImg.tsx
@@ -0,0 +1,24 @@
+
+const BannerImg = () => {
+  return (
+    <div className="relative">
+    <img
+      src="/img/banner/star.png"
+      alt="star"
+      className="absolute top-0 left-0"
+    />
+    <img
+      src="/img/banner/ellipse.png"
+      alt="ellipse"
+      className="w-full h-auto"
+    />
+    <img
+      src="/img/banner/guy_img.png"
+      alt="guy_img"
+      className="w-[255px] h-[382px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+    />
+  </div>
+  )
+}
+
+export default BannerImg
