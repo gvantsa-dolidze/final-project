6c9045b5121f746b5fd2535add1463a43b1dc68e
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-12 11:31:48 +0400

    Update AdminSideBar component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,54 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
-import Featured from './components/homepage_components/Featured';
-import Latest from './components/homepage_components/Latest';
-import ProductPage from './pages/ProductPage';
+import Featured from "./components/homepage_components/Featured";
+import Latest from "./components/homepage_components/Latest";
+import ProductPage from "./pages/ProductPage";
 import DetailsContent from "./components/product_page_components/DetailsContent";
 import ReviewsContent from "./components/product_page_components/ReviewsContent";
-import ListingPage from './pages/ListingPage';
-import CartPage from './pages/CartPage';
-import CheckoutPage from './pages/CheckoutPage';
-import AfterPaymentPage from './pages/AfterPaymentPage'
-import LoginPage from './pages/LoginPage';
-import SignUpPage from './pages/SignUpPage';
-import ForgotPassword from './pages/ForgotPassword';
-import ResetPassword from './pages/ResetPassword';
-import ProfilePage from './pages/ProfilePage';
-import Orders from './components/profile_pages_components/Orders';
-import WishList from './components/profile_pages_components/WishList';
-import Address from './components/profile_pages_components/Address';
-import AccountDetail from './components/profile_pages_components/AccountDetail';
-import Password from './components/profile_pages_components/Password';
-import Logout from './components/profile_pages_components/Logout';
+import ListingPage from "./pages/ListingPage";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import AfterPaymentPage from "./pages/AfterPaymentPage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import ProfilePage from "./pages/ProfilePage";
+import Orders from "./components/profile_pages_components/Orders";
+import WishList from "./components/profile_pages_components/WishList";
+import Address from "./components/profile_pages_components/Address";
+import AccountDetail from "./components/profile_pages_components/AccountDetail";
+import Password from "./components/profile_pages_components/Password";
+import Logout from "./components/profile_pages_components/Logout";
 
-import About from "./pages/About"
-import AddProduct from "./admin/admin_pages/AddProduct"
-import AdminPage from "./admin/admin_pages/AdminPage"
-import Customers from "./admin/admin_pages/Customers"
-import Dashboard from "./admin/admin_pages/Dashboard"
-import Login from "./admin/admin_pages/Login"
-import AdminsOrders from "./admin/admin_pages/AdminsOrders"
-import Products from "./admin/admin_pages/Products"
-import Reviews from "./admin/admin_pages/Reviews"
-import Settings from "./admin/admin_pages/Settings"
+import About from "./pages/About";
+import AddProduct from "./admin/admin_pages/AddProduct";
+import AdminPage from "./admin/admin_pages/AdminPage";
+import Customers from "./admin/admin_pages/Customers";
+import Dashboard from "./admin/admin_pages/Dashboard";
+import Login from "./admin/admin_pages/Login";
+import AdminsOrders from "./admin/admin_pages/AdminsOrders";
+import Products from "./admin/admin_pages/Products";
+import Reviews from "./admin/admin_pages/Reviews";
+import Settings from "./admin/admin_pages/Settings";
 
 function App() {
   return (
     <div className="App">
-      
       <Routes>
         {/* homepage routing */}
         <Route path="/" element={<Home />}>
-          <Route index element={ <Featured /> } />
+          <Route index element={<Featured />} />
           <Route path="featured" element={<Featured />} />
           <Route path="latest" element={<Latest />} />
         </Route>
 
-       {/* productpage routing */}
-        <Route path="/product_page" element={<ProductPage />} >
-           <Route index element={ <DetailsContent /> } />
-           <Route path="details_content" element={<DetailsContent />} />
-           <Route path="reviews_content" element={<ReviewsContent />} />
+        {/* productpage routing */}
+        <Route path="/product_page" element={<ProductPage />}>
+          <Route index element={<DetailsContent />} />
+          <Route path="details_content" element={<DetailsContent />} />
+          <Route path="reviews_content" element={<ReviewsContent />} />
         </Route>
 
         <Route path="/listing_page" element={<ListingPage />} />
@@ -62,27 +61,29 @@ function App() {
         <Route path="/reset_password_page" element={<ResetPassword />} />
         <Route path="/about" element={<About />} />
 
-          {/* profile Page routing */}
-         <Route path="/profile_page" element={<ProfilePage />} >
-           <Route index element={ <Orders /> } />
-           <Route path="orders" element={<Orders />} />
-           <Route path="wishlists" element={<WishList />} />
-           <Route path="address" element={<Address />} />
-           <Route path="password" element={<Password />} />
-           <Route path="account_detail" element={<AccountDetail />} />
-           <Route path="logout" element={<Logout />} />
-         </Route>
-        
+        {/* profile Page routing */}
+        <Route path="/profile_page" element={<ProfilePage />}>
+          <Route index element={<Orders />} />
+          <Route path="orders" element={<Orders />} />
+          <Route path="wishlists" element={<WishList />} />
+          <Route path="address" element={<Address />} />
+          <Route path="password" element={<Password />} />
+          <Route path="account_detail" element={<AccountDetail />} />
+          <Route path="logout" element={<Logout />} />
+        </Route>
+
         {/* admin's Page routing */}
-        <Route path="/admin_page" element={<AdminPage />} >
-           <Route index element={ <Dashboard /> } />
-           <Route path="dashboard" element={<Dashboard />} />
-           <Route path="products" element={<Products />} />
-           <Route path="admins_orders" element={<AdminsOrders />} />
-           <Route path="customers" element={<Customers />} />
-           <Route path="reviews" element={<Reviews />} />
-           <Route path="settings" element={<Settings />} />
-         </Route>
+        <Route path="/admin_page" element={<AdminPage />}>
+          <Route index element={<Dashboard />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="products" element={<Products />} />
+          <Route path="admins_orders" element={<AdminsOrders />} />
+          <Route path="customers" element={<Customers />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="settings" element={<Settings />} />
+        </Route>
+        <Route path="add_product" element={<AddProduct />} />
+        <Route path="login" element={<Login />} />
       </Routes>
     </div>
   );
--- src/admin/admin_components/AdminSideBar.tsx
+++ src/admin/admin_components/AdminSideBar.tsx
@@ -1,6 +1,3 @@
-// import { NavLink } from "react-router-dom"
-// import SideBarNavLink from "../../components/elements/SideBarNavLink";
-
 import LinkBtn from "../../components/elements/LinkBtn";
 import AdminLogo from "../admin_elements/AdminLogo";
 const AdminSideBar = () => {
@@ -8,63 +5,55 @@ const AdminSideBar = () => {
     <div className="flex flex-col w-64 border-b-2 p-5 bg-white border-r ">
       <AdminLogo />
       <div className="flex flex-col border-b pb-5">
-        <LinkBtn  destination='dashboard' 
-                  label='Dashboard' 
-                  variant='sidebar' 
-                  img='/img/icons/Dashboard.png'
-                  />
-        
-        {/* <NavLink
-          to="dashboard"
-          className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md"
-        >
-          <img src="/img/icons/Add to cart.png" alt="img" /> <p>Dashboard</p>
-        </NavLink>
-        <NavLink
-          to="products"
-          className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md"
-        >
-          {" "}
-          <img src="/img/icons/Add to cart.png" alt="img" /> <p>Products</p>{" "}
-        </NavLink>
-        <NavLink
-          to="admins_orders"
-          className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md"
-        >
-          <img src="/img/icons/Add to cart.png" alt="img" /> <p>Orders</p>{" "}
-        </NavLink>
-        <NavLink
-          to="customers"
-          className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md"
-        >
-          <img src="/img/icons/Add to cart.png" alt="img" /> <p>Customers</p>{" "}
-        </NavLink>
-        <NavLink
-          to="reviews"
-          className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md"
-        >
-          {" "}
-          <img src="/img/icons/Add to cart.png" alt="img" />
-          <p>reviews</p>
-        </NavLink>
-        <NavLink
-          to="settings"
-          className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md"
-        >
-          {" "}
-          <img src="/img/icons/Add to cart.png" alt="img" />
-          <p>settings</p>
-        </NavLink> */}
+        <LinkBtn
+          destination="dashboard"
+          label="Dashboard"
+          variant="sidebar"
+          img="/img/icons/Dashboard.png"
+        />
+
+        <LinkBtn
+          destination="products"
+          label="Product"
+          variant="sidebar"
+          img="/img/icons/product.png"
+        />
+
+        <LinkBtn
+          destination="admins_orders"
+          label="Orders"
+          variant="sidebar"
+          img="/img/icons/Cart.png"
+        />
+
+        <LinkBtn
+          destination="customers"
+          label="Customers"
+          variant="sidebar"
+          img="/img/icons/Users.png"
+        />
+
+        <LinkBtn
+          destination="reviews"
+          label="Reviews"
+          variant="sidebar"
+          img="/img/icons/Empty Star.png"
+        />
+
+        <LinkBtn
+          destination="settings"
+          label="Settings"
+          variant="sidebar"
+          img="/img/icons/Settings.png"
+        />
       </div>
       <div className="py-5">
-        {/* <NavLink
-          to=""
-          className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md"
-        >
-          <img src="/img/icons/Add.png" alt="" />
-          <p>Extras</p>
-        </NavLink> */}
-        <LinkBtn destination='' label='Extras' variant='sidebar' img='/img/icons/Add.png'/>
+        <LinkBtn
+          destination=""
+          label="Extras"
+          variant="sidebar"
+          img="/img/icons/Add.png"
+        />
       </div>
     </div>
   );
