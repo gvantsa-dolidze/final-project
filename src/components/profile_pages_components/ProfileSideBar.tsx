10b957a2696524c594a0bafebe660b4541a8b37a
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-12 11:52:49 +0400

    Implement dynamic ProfileSideBar using LinkBtn component

--- src/components/profile_pages_components/ProfileSideBar.tsx
+++ src/components/profile_pages_components/ProfileSideBar.tsx
@@ -1,28 +1,47 @@
-import { NavLink } from "react-router-dom"
+import { NavLink } from "react-router-dom";
+import LinkBtn from "../elements/LinkBtn";
 
 const ProfileSideBar = () => {
   return (
     <div className="flex flex-col w-1/4 border-r-2 p-5">
-        <NavLink to="orders" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
-          <img src="/img/icons/Add to cart.png" alt="img" /> <p>Orders</p>
-        </NavLink>
-        <NavLink to="wishlists" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
-          <img src="/img/icons/Heart.png" alt="img" /> <p>Wishlists</p>
-        </NavLink>
-        <NavLink to="address" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
-          <img src="/img/icons/Delivery.png" alt="img" /> <p>Address</p>
-        </NavLink>
-        <NavLink to="password" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
-          <img src="/img/icons/Key.png" alt="img" /> <p>Password</p>
-        </NavLink>
-        <NavLink to="account_detail" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
-          <img src="/img/icons/User.png" alt="img" /> <p>Account Detail</p>
-        </NavLink>
-        <NavLink to="logout" className="p-4 flex justify-start gap-5 hover:bg-BackgroundGray hover:rounded-md">
-          <img src="/img/icons/Logout.png" alt="img" /> <p>Logout</p>
-        </NavLink>
-      </div>
-  )
-}
+      <LinkBtn
+        destination="orders"
+        label="Orders"
+        variant="sidebar"
+        img="/img/icons/Add to cart.png"
+      />
+      <LinkBtn
+        destination="wishlists"
+        label="Wishlists"
+        variant="sidebar"
+        img="/img/icons/Heart.png"
+      />
+      <LinkBtn
+        destination="address"
+        label="Address"
+        variant="sidebar"
+        img="/img/icons/Delivery.png"
+      />
+      <LinkBtn
+        destination="password"
+        label="Password"
+        variant="sidebar"
+        img="/img/icons/Key.png"
+      />
+      <LinkBtn
+        destination="account_detail"
+        label="Account Detail"
+        variant="sidebar"
+        img="/img/icons/User.png"
+      />
+      <LinkBtn
+        destination="logout"
+        label="Logout"
+        variant="sidebar"
+        img="/img/icons/Logout.png"
+      />
+    </div>
+  );
+};
 
-export default ProfileSideBar
+export default ProfileSideBar;
