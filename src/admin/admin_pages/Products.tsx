cc5e99d08a4b54016a425e4fd91541e4834edbd6
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-16 14:32:34 +0400

    Import Pagination component in Product component

--- src/admin/admin_pages/Products.tsx
+++ src/admin/admin_pages/Products.tsx
@@ -1,3 +1,4 @@
+import Pagination from "../../components/elements/Pagination";
 import AdminSearchBart from "../admin_components/AdminSearchBar";
 import AdminTable from "../admin_components/AdminTable";
 
@@ -6,6 +7,10 @@ const Products = () => {
     <div className="bg-white rounded-md border">
       <AdminSearchBart />
       <AdminTable />
+      <div className="flex justify-end">
+      <Pagination />  
+      </div>
+      
     </div>
   );
 };
