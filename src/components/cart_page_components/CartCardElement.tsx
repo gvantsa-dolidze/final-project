d05ead186ff41ed69c59bd68e7141d92c992d4b4
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-16 22:39:10 +0400

    import Counter in CartCardElement.tsx

--- src/components/cart_page_components/CartCardElement.tsx
+++ src/components/cart_page_components/CartCardElement.tsx
@@ -1,26 +1,27 @@
-import Button from "../elements/Button"
-import ImgElement from "../elements/ImgElement"
+import { Counter } from "../../store/app/Counter";
+import Button from "../elements/Button";
+import ImgElement from "../elements/ImgElement";
 
 const CartCardElement = () => {
   return (
     <div className="flex items-center justify-between mt-5">
-        <div className="flex gap-5">
-          <ImgElement />
-          <div className="text-start">
-            <h3>Raw Black T-Shirt Lineup</h3>
-            <div className="flex gap-2 items-center">
-              color: <div className="bg-green-700 w-3 h-3 rounded-full"></div> -
-              Size: M
-            </div>
+      <div className="flex gap-5">
+        <ImgElement />
+        <div className="text-start">
+          <h3>Raw Black T-Shirt Lineup</h3>
+          <div className="flex gap-2 items-center">
+            color: <div className="bg-green-700 w-3 h-3 rounded-full"></div> -
+            Size: M
           </div>
         </div>
-        <div className="flex gap-5">
-          <div>$75.00</div>
-          <div> - 1 + </div>
-          <Button  variant='gray' img='/img/icons/x.png'/>
-        </div>
       </div>
-  )
-}
+      <div className="flex items-center gap-5">
+        <div>$75.00</div>
+        <Counter />
+        <Button variant="gray" img="/img/icons/x.png" />
+      </div>
+    </div>
+  );
+};
 
-export default CartCardElement
+export default CartCardElement;
