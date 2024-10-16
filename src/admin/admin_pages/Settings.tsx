6e19af4d16770b6db8d710282d53407fd046ff5f
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-16 18:08:08 +0400

    Update Settings component

--- src/admin/admin_pages/Settings.tsx
+++ src/admin/admin_pages/Settings.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import InputElement from "../../components/elements/InputElement";
 
 const Settings = () => {
   return (
-    <div>Settings</div>
-  )
-}
+    <div className="bg-white rounded-md border">
+      <div className="border-b p-7 text-start">Settings</div>
+      <form action="" className="w-96 px-7 pb-52 pt-16 space-y-7">
+        <InputElement type="text" text="Site Name" />
+        <InputElement type="email" text="Support Email" />
+        <InputElement type="email" text="Monthly Order Goal" />
+      </form>
+    </div>
+  );
+};
 
-export default Settings
+export default Settings;
