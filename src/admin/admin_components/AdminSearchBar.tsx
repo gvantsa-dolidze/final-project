eeac06d5010e2fa0c294d34d71aee40cddee443f
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-16 14:44:04 +0400

    Add showButton prop to AdminSearchBar for conditional button rendering

--- src/admin/admin_components/AdminSearchBar.tsx
+++ src/admin/admin_components/AdminSearchBar.tsx
@@ -1,12 +1,16 @@
 import Button from "../../components/elements/Button"
 import SearchBar from "../../components/header_components/SearchBar"
 
-const AdminSearchBar = () => {
+interface AdminSearchBarProps {
+  showButton?: boolean; // Optional prop
+}
+
+const AdminSearchBar: React.FC<AdminSearchBarProps> = ({ showButton }) => {
   return (
     <div className="flex justify-between p-7 items-center">
     <h3>Products</h3>
     <div className="flex gap-5">
-      <Button label="Add product" />
+    {showButton && <Button label="Add product" />}
       <SearchBar />
     </div>
   </div>
--- src/admin/admin_pages/AdminsOrders.tsx
+++ src/admin/admin_pages/AdminsOrders.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
-
+import Pagination from "../../components/elements/Pagination";
+import AdminSearchBart from "../admin_components/AdminSearchBar";
+import AdminTable from "../admin_components/AdminTable";
 const AdminsOrders = () => {
   return (
-    <div>AdminsOrders</div>
+    <div className="bg-white rounded-md border">
+      <AdminSearchBart  showButton={false}/>
+      <AdminTable />
+      <div className="flex justify-end">
+      <Pagination /> 
+      </div>
+    </div>
   )
 }
 
