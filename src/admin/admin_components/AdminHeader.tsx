2b9609bb52769a045bd6ee61ba10fba639798ed8
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-16 11:01:23 +0400

    Add optional secondaryLabel and destination props to Breadcrumb component

--- src/admin/admin_components/AdminHeader.tsx
+++ src/admin/admin_components/AdminHeader.tsx
@@ -1,13 +1,18 @@
-import Breadcrumb from "../../components/elements/Breadcrumb"
-import Button from "../../components/elements/Button"
+import Breadcrumb from "../../components/elements/Breadcrumb";
+import Button from "../../components/elements/Button";
 
 const AdminHeader = () => {
   return (
     <div className="flex justify-between p-7">
-        <Breadcrumb label='Dashboard' title=''/>
-        <Button  img="/img/icons/Logout.png" variant="gray"/>
+      <Breadcrumb
+        label="Admin"
+        secondaryLabel="Dashboard"
+        destination="/admin_page"
+        secondaryDestination="/admin_page/dashboard"
+      />
+      <Button img="/img/icons/Logout.png" variant="gray" />
     </div>
-  )
-}
+  );
+};
 
-export default AdminHeader
+export default AdminHeader;
--- src/components/elements/Breadcrumb.tsx
+++ src/components/elements/Breadcrumb.tsx
@@ -2,22 +2,28 @@ import LinkBtn from "./LinkBtn";
 
 interface BreadcrumbProps {
   label: string;
-  title: string;
+  title?: string;
+  secondaryLabel?: string;
+  destination?: string;
+  secondaryDestination?: string;
 }
 
-const Breadcrumb: React.FC<BreadcrumbProps> = ({ label, title }) => {
+const Breadcrumb: React.FC<BreadcrumbProps> = ({ label, title, secondaryLabel = '', destination='', secondaryDestination=''}) => {
   return (
     <div className="max-w-1116 py-5 ">
       <h3 className="text-start pb-4 text-xl">{title}</h3>
       <nav className="flex" aria-label="Breadcrumb">
         <ol className="flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
           <li className="inline-flex items-center">
-            <LinkBtn destination="/" label="Ecommerce" variant="link" />
+            <LinkBtn 
+                  destination={destination} 
+                  label={label} 
+                  variant="link" />
           </li>
           <li>
               <LinkBtn
-                destination="/"
-                label={label}
+                destination={secondaryDestination}
+                label={secondaryLabel}
                 variant="link"
                 img="/img/icons/Chevron Right.png"
               />
