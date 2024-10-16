2763dc2de06d4feadf20e101a7ff1099ae16ad0d
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-16 15:49:10 +0400

    Delete AdminTable component

--- src/admin/admin_pages/AdminsOrders.tsx
+++ src/admin/admin_pages/AdminsOrders.tsx
@@ -1,11 +1,10 @@
 import Pagination from "../../components/elements/Pagination";
 import AdminSearchBar from "../admin_components/AdminSearchBar";
-import AdminTable from "../admin_components/AdminTable";
+
 const AdminsOrders = () => {
   return (
     <div className="bg-white rounded-md border">
       <AdminSearchBar  showButton={false}/>
-      <AdminTable />
       <div className="flex justify-end">
       <Pagination /> 
       </div>
