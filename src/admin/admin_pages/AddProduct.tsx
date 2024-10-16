79f23bd294daae66ed445bbc5d0e1370229d2050
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-16 19:33:47 +0400

    Update AddProduct component

--- src/admin/admin_pages/AddProduct.tsx
+++ src/admin/admin_pages/AddProduct.tsx
@@ -1,8 +1,42 @@
+import Button from "../../components/elements/Button";
+import ColorPiker from "../../components/elements/ColorPiker";
+import InputElement from "../../components/elements/InputElement";
+import SizePiker from "../../components/elements/SizePiker";
 
 const AddProduct = () => {
   return (
-    <div>AddProduct</div>
-  )
-}
+    <div className="bg-white rounded-md border">
+      <div className="text-start p-7 border-b">Add Product</div>
+      <form action="">
+        <div className="flex gap-10 p-10">
+          <div className="w-72 space-y-3">
+            <InputElement type="text" text="Title" />
+            <InputElement type="text" text="Category" />
+            <InputElement type="number" text="Price" />
+            <InputElement type="text" text="Slug" />
+            <InputElement type="text" text="SKU" />
+            <textarea className="outline-none border rounded-md w-72 h-32" />
+          </div>
+          <div className="w-72 space-y-5">
+            <InputElement type="text" text="Stock status" />
+            <InputElement type="text" text="Available quantity" />
+            <InputElement type="file" text="Images" />
+            <div>
+              <h3 className="text-start">Colors</h3>
+              <ColorPiker />
+            </div>
+            <div>
+              <h3 className="text-start">Size</h3>
+              <SizePiker />
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-start pl-10 pb-5">
+          <Button label="Save Product" />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default AddProduct
+export default AddProduct;
