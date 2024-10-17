a0cb35bcd4be5ae22737c9ac818a220373ecd2dc
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-17 17:40:49 +0400

    changes

--- src/components/cart_page_components/CartCardElement.tsx
+++ src/components/cart_page_components/CartCardElement.tsx
@@ -8,18 +8,13 @@ import ColorSizeDisplay from "./ColorSizeDisplay";
 interface CartCardElementProps {
   itemId: string; // Adjust type as necessary (string or number)
 }
-
 const CartCardElement: React.FC<CartCardElementProps> = ({ itemId }) => {
   return (
     <div className="flex items-center justify-between mt-5">
       <div className="flex gap-5">
         <ImgElement />
         <div className="text-start">
-          <ProductTItle title="Raw Black T-Shirt Lineup" />
-          {/* <div className="flex gap-2 items-center">
-            color: <div className="bg-green-700 w-3 h-3 rounded-full"></div> -
-            Size: M
-          </div> */}
+          <ProductTItle />
           <ColorSizeDisplay />
         </div>
       </div>
