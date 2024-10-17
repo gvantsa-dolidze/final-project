ac22dd1a4ef072ecd6187824a3ea24d0e23a03d8
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-17 08:58:58 +0400

    Create SelectSizeCheckBtn element

---
+++ src/components/elements/SelectSizeCheckBtn.tsx
@@ -0,0 +1,27 @@
+interface SelectSizeCheckBtnProps {
+    variant?: "s" | "m" | "l" | "xl" | "xxl";
+  }
+  const SelectSizeCheckBtn: React.FC<SelectSizeCheckBtnProps> = ({
+    variant = "s",
+  }) => {
+    const styles: Record<string, string> = {
+    };
+  
+    return (
+      <div className="relative flex items-center justify-center">
+        <label htmlFor={`colorPiker-${variant}`} className="absolute cursor-pointer text-sm">
+          {variant}
+        </label>
+        <input
+          type="check"
+          className={`${styles[variant]} appearance-none w-10 h-10 border rounded cursor-pointer checked:border-[black] hover:bg-BackgroundGray`}
+          name="size-piker"
+          id={`colorPiker-${variant}`}
+          value={variant}
+        />
+      </div>
+    );
+  };
+  
+  export default SelectSizeCheckBtn;
+  
