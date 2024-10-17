8e24b80b42c18bf29879b4c4d03bbf985e3fed79
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-17 18:16:51 +0400

    update title component

--- src/components/product_page_components/TItle.tsx
+++ src/components/product_page_components/TItle.tsx
@@ -1,8 +1,20 @@
 interface TitleProps {
   title?: string; // Title of the product
+  variant?: "PageTitle" | "ProductTitle" | "ElementTitle";
+  className?: string;
 }
-const TItle: React.FC<TitleProps> = ({ title = 'Raw Black T-Shirt Lineup' }) => {
-  return <h3>{title}</h3>;
+const Title: React.FC<TitleProps> = ({
+  title = "Raw Black T-Shirt Lineup",
+  variant = "ElementTitle",
+  className = "",
+}) => {
+  const styles: Record<string, string> = {
+    PageTitle: "",
+    ProductTitle:"",
+    ElementTitle: "",
+  };
+
+  return <h3 className={`${styles[variant]} ${className}`}>{title}</h3>;
 };
 
-export default TItle;
+export default Title;
