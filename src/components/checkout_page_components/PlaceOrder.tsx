dc7fbba8c6e3e774af4d00d3a883677e86795858
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-17 16:07:50 +0400

    update PlaceOrder with ProductPrice component

--- src/components/checkout_page_components/PlaceOrder.tsx
+++ src/components/checkout_page_components/PlaceOrder.tsx
@@ -1,21 +1,22 @@
 import Button from "../elements/Button"
+import ProductPrice from "../elements/ProductPrice"
 
 const PlaceOrder = () => {
   return (
     <div className=" py-10 px-2  flex flex-col w-96 gap-5 ">
     <div className="border-b space-y-5 py-5">
       <div className="flex justify-between">
-        <p>Subtotal</p> <p>$ 90.00</p>
+        <p>Subtotal</p> <ProductPrice />
       </div>
       <div className="flex justify-between">
         <p>Shipping</p> <p>Free</p>
       </div>
       <div className="flex justify-between">
-        <p>Tax</p> <p>$ 3.00</p>
+        <p>Tax</p> <ProductPrice />
       </div>
     </div>
     <div className="flex justify-between">
-      <p>Total</p> <p>$ 100.00</p>
+      <p>Total</p> <ProductPrice />
     </div>
     <div className="flex flex-col gap-7">
       <Button label="Place Order" />
