52b3052e7ef8b9bd431a67ce946ae7a2df446371
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-17 18:55:38 +0400

    Update Breadcrumb with Title component

--- src/components/elements/Breadcrumb.tsx
+++ src/components/elements/Breadcrumb.tsx
@@ -1,3 +1,4 @@
+import Title from "../product_page_components/Title";
 import LinkBtn from "./LinkBtn";
 
 interface BreadcrumbProps {
@@ -11,7 +12,7 @@ interface BreadcrumbProps {
 const Breadcrumb: React.FC<BreadcrumbProps> = ({ label='', title, secondaryLabel = '', destination='', secondaryDestination=''}) => {
   return (
     <div className="max-w-1116 py-5 ">
-      <h3 className="text-start pb-4 text-xl">{title}</h3>
+      <Title title={title} className="text-start pb-4 text-xl" variant="title24"/>
       <nav className="flex" aria-label="Breadcrumb">
         <ol className="flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
           <li className="inline-flex items-center">
