508855fde106b04b5c15473ee0774b3cb9a967e0
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-19 03:38:55 +0400

    Add burger button

--- src/components/header_components/Header.tsx
+++ src/components/header_components/Header.tsx
@@ -1,3 +1,4 @@
+import Button from "../elements/Button";
 import HeaderLogo from "./HeaderLogo";
 import HeaderNav from "./HeaderNav";
 import LoginAndCart from "./LoginAndCart";
@@ -10,15 +11,16 @@ const Header = () => {
       <div className="flex items-center justify-between gap-5 max-w-1116 m-auto pt-3 pb-3">
         <div className="flex items-center w-[641px] justify-between gap-5">
           <HeaderLogo />
-          <HeaderNav
-            home="Home"
-            about="About"
-            contact="Contact"
-          />
+          <HeaderNav home="Home" about="About" contact="Contact" />
         </div>
         <div className="flex w-[370px] justify-end gap-5">
           <SearchBar />
           <LoginAndCart />
+          <Button
+            className=" md:hidden block w-10"
+            variant="link"
+            img="/img/icons/Menu.png"
+          />
         </div>
       </div>
     </div>
