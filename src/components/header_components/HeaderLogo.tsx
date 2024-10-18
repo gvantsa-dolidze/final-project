13f9725659817edd48d17ca565c45cf855713b06
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-19 03:25:13 +0400

    changes

--- src/components/header_components/HeaderLogo.tsx
+++ src/components/header_components/HeaderLogo.tsx
@@ -3,7 +3,9 @@ import { NavLink } from "react-router-dom";
 const HeaderLogo = () => {
   return (
     <NavLink to="/" className="flex items-center gap-1">
-      <img src="/img/logo/Logomark.png" alt="logo" /> <span>Ecommerce</span>
+      <img src="/img/logo/Logomark.png" alt="logo" />
+
+      <span className="hidden lg:block ">Ecommerce</span>
     </NavLink>
   );
 };
