1c8c94ac74a0812d577cc6ecd2c57d73521c2fb5
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-19 03:27:59 +0400

    changes

--- src/components/header_components/LoginAndCart.tsx
+++ src/components/header_components/LoginAndCart.tsx
@@ -2,7 +2,7 @@ import LinkBtn from "../elements/LinkBtn";
 
 const LoginAndCart = () => {
   return (
-    <div className="flex items-center">
+    <div className="md:flex hidden items-center">
       <LinkBtn destination='/cart_page' img="/img/icons/cart.png" variant='link' className="w-10 flex justify-center"/>
       <LinkBtn destination='/login_page' img="/img/icons/user.png" variant='link' className="w-10 flex justify-center"/>
     </div>
