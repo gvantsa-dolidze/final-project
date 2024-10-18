0ccfa5f2c9c7d765d6e3d2be316f36c2e78cda6a
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-19 02:22:14 +0400

    Update SearchBar component

--- src/components/header_components/SearchBar.tsx
+++ src/components/header_components/SearchBar.tsx
@@ -1,14 +1,14 @@
-import InputElement from "../elements/InputElement"
+import InputElement from "../elements/InputElement";
 
 const SearchBar = () => {
   return (
-    <form className="border rounded-md px-3 flex items-center">
-        <div className="flex items-center gap-2">
-            <img src="/img/icons/Search.png" alt="" />
-            <InputElement variant='noBorder' type='text'/>
-        </div>
-    </form>
-  )
-}
+    <div className=" border rounded-md px-3  flex items-center gap-2 justify-between">
+      <img src="/img/icons/Search.png" alt="" />
+      <form>
+        <InputElement variant="noBorder" type="text" />
+      </form>
+    </div>
+  );
+};
 
-export default SearchBar
+export default SearchBar;
