8ac27d5737f06fd6a3ad624ff3c3d3c9c194d62e
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-20 13:25:13 +0400

    import useNavigate and update onSubmit function with navigate to ListingPage

--- src/components/header_components/SearchBar.tsx
+++ src/components/header_components/SearchBar.tsx
@@ -1,20 +1,23 @@
 import InputElement from "../elements/InputElement";
 import { useState } from "react";
-
-
+import { useNavigate } from "react-router";
 
 const SearchBar = () => {
-
 const  [value, setValue] = useState<string>('');
-
+const navigate = useNavigate();
 const onChangeSearch = (e: any) => {
   setValue(e.target.value);
 }
 
+const onSubmit = (e: any) => {
+  e.preventDefault()
+  console.log(value)
+  navigate("listing_page")
+}
   return (
     <div className="border rounded-md px-3 py-1 w-56 flex items-center gap-2 justify-start">
       <img src="/img/icons/Search.png" alt="" />
-      <form>
+      <form onSubmit={onSubmit}>
         <InputElement variant="noBorder" type="text" onChange={onChangeSearch}/>
       </form>
     </div>
