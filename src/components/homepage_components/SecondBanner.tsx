febbf55c9036ebc011b252e19abb34492f479442
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-21 15:18:53 +0400

    Update SecondBanner

--- src/components/homepage_components/SecondBanner.tsx
+++ src/components/homepage_components/SecondBanner.tsx
@@ -3,8 +3,10 @@ import LinkBtn from "../elements/LinkBtn";
 const SecondBanner = () => {
   return (
     <div className="bg-BackgroundGray py-5">
-      <div className="text-start max-w-1116 m-auto space-y-5 flex justify-between">
-        <div className="space-y-5 my-auto flex flex-col w-96 items-center md:items-start ">
+      {/* <div className="text-start max-w-1116 m-auto space-y-5 flex justify-between">
+        <div className="space-y-5 my-auto flex flex-col w-96 items-center md:items-start "> */}
+        <div className="text-start max-w-1116 m-auto flex md:justify-between justify-center items-center p-2">
+        <div className="space-y-5 my-auto flex flex-col w-96 md:items-start items-center">
           <h2>Browse Our Fashion Paradise!</h2>
           <p className="pb-6 text-center md:text-start">
             Step into a world of style and explore our diverse collection of
