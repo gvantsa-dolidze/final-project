327bf2ce0a1186d24d2002adcc47b2d035118be4
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-30 12:10:35 +0400

    fixed bag

--- src/components/product_page_components/DetailsAndReviews.tsx
+++ src/components/product_page_components/DetailsAndReviews.tsx
@@ -3,8 +3,8 @@ import LinkBtn from "../elements/LinkBtn";
 const DetailsAndReviews = () => {
   return (
     <div className="space-y-5 w-[241px]">
-      <LinkBtn destination='details_content' img='/img/icons/More.png' label='Details' variant='sidebar'/>
-      <LinkBtn destination='reviews_content' img='/img/icons/Empty Star.png' label='Reviews' variant='sidebar'/>
+      <LinkBtn destination='details_content' img='/img/icons/more.png' label='Details' variant='sidebar'/>
+      <LinkBtn destination='reviews_content' img='/img/icons/empty_star.png' label='Reviews' variant='sidebar'/>
     </div>
   );
 };
