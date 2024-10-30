83a2a443b6b5fbe23e080fa0c8c5171ff54e4268
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-30 15:33:32 +0400

    Update DetailsContent

--- src/components/product_page_components/DetailsContent.tsx
+++ src/components/product_page_components/DetailsContent.tsx
@@ -1,23 +1,16 @@
-const DetailsContent = () => {
+interface DetailsContentProps {
+  description: string;
+}
+
+const DetailsContent: React.FC<DetailsContentProps> = ({description}) => {
+  const styles: Record<string, string> = {};
   return (
     <div className="text-start space-y-10 w-[727px]">
       <h2>Details</h2>
       <div className="space-y-20">
         <p>
-          Elevate your everyday style with our Men's Black T-Shirts, the
-          ultimate wardrobe essential for modern men. Crafted with meticulous
-          attention to detail and designed for comfort, these versatile black
-          tees are a must-have addition to your collection. The classic black
-          color never goes out of style. Whether you're dressing up for a
-          special occasion or keeping it casual, these black t-shirts are the
-          perfect choice, effortlessly complementing any outfit.
+          {description}
         </p>
-        <ul>
-          <li>Premium Quality</li>
-          <li>Versatile Wardrobe Staple</li>
-          <li>Available in Various Sizes</li>
-          <li>Tailored Fit</li>
-        </ul>
       </div>
     </div>
   );
