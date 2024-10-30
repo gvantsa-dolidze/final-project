3dc650ebc7a00f9e428684de8594cc576fe0495a
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-30 15:13:52 +0400

    Update ReviewAndSort component with id

--- src/components/product_page_components/ReviewAndSort.tsx
+++ src/components/product_page_components/ReviewAndSort.tsx
@@ -1,13 +1,16 @@
+import { useParams } from "react-router";
 import LinkBtn from "../elements/LinkBtn";
 import ProductRatingInfo from "../elements/ProductRatingInfo";
 import SortByBtn from "../elements/SortByBtn";
+
 const ReviewAndSort = () => {
+  const { id } = useParams(); 
   return (
     <div className="flex justify-between items-end border-b pb-10">
       <div className="space-y-5 text-start">
         <h2>Reviews</h2>
         <ProductRatingInfo />
-        <LinkBtn label="Write a review" destination="/product_page/write_review"/>
+        <LinkBtn label="Write a review" destination={`/product_page/${id}/write_review`}/>
       </div>
       <SortByBtn />
     </div>
