c6cd032b8477c93a7985d1b3a462b08ff615fe56
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-10-30 12:14:44 +0400

    changes

--- src/components/elements/FiveStar.tsx
+++ src/components/elements/FiveStar.tsx
@@ -2,11 +2,11 @@
 const FiveStar = () => {
   return (
     <div className="flex">
-        <img src="/img/icons/Empty Star.png" alt="star" className="w-6 h-6" />
-        <img src="/img/icons/Empty Star.png" alt="star" className="w-6 h-6"/>
-        <img src="/img/icons/Empty Star.png" alt="star" className="w-6 h-6"/>
-        <img src="/img/icons/Empty Star.png" alt="star" className="w-6 h-6"/>
-        <img src="/img/icons/Empty Star.png" alt="star" className="w-6 h-6"/>
+        <img src="/img/icons/empty_star.png" alt="star" className="w-6 h-6" />
+        <img src="/img/icons/empty_star.png" alt="star" className="w-6 h-6"/>
+        <img src="/img/icons/empty_star.png" alt="star" className="w-6 h-6"/>
+        <img src="/img/icons/empty_star.png" alt="star" className="w-6 h-6"/>
+        <img src="/img/icons/empty_star.png" alt="star" className="w-6 h-6"/>
     </div>
   )
 }
