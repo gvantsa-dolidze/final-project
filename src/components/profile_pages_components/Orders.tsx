679d72ea67920ec8e5bad089d8c53a1d2ca9e9d7
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-11-06 16:51:14 +0400

    Update Orders and OrdersCardElement Components

--- src/components/profile_pages_components/Orders.tsx
+++ src/components/profile_pages_components/Orders.tsx
@@ -1,12 +1,25 @@
 import OrdersCardElement from "./OrdersCardElement";
 
 
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { useEffect } from "react";
+import { getAllProducts } from "../../store/app/AllProductsReducer";
+
 const Orders = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  });
+
+  const products = useAppSelector((state) => state.allProduct.data || []);
+
   return (
     <div className="flex  flex-col pl-10 space-y-10">
       <h3 className="text-start">Orders</h3>
-      <OrdersCardElement />
-      <OrdersCardElement />
+      {products.map((product: any) => (
+      <OrdersCardElement title={product.title} image={product.image} price={product.price}/>
+      ))}
     </div>
   );
 };
--- src/components/profile_pages_components/OrdersCardElement.tsx
+++ src/components/profile_pages_components/OrdersCardElement.tsx
@@ -1,19 +1,32 @@
 import Button from "../elements/Button";
 import ImgElement from "../elements/ImgElement";
+import ProductPrice from "../elements/ProductPrice";
+import ProductTItle from "../product_page_components/Title";
 
-const OrdersCardElement = () => {
+interface OrdersCardElementProps {
+  // id: string; 
+  
+  image: string;
+  title: string;
+  price: number;
+  
+}
+
+const OrdersCardElement: React.FC<OrdersCardElementProps> = ({ title, price, image }) => {
   return (
     <div className="flex items-center justify-between w-full">
       <div className="flex gap-5">
-        <ImgElement />
+        <ImgElement image={image} />
         <div className="text-start">
-          <h3>Raw Black T-Shirt Lineup</h3>
+          {/* <h3>Raw Black T-Shirt Lineup</h3> */}
+          <ProductTItle title={title}/>
           <p>Added on: 27 July 2023</p>
           <Button variant="link" label="Remove item"  />
         </div>
       </div>
       <div className="flex gap-5 items-center">
-        <div>$ 75.00</div>
+        {/* <div>$ 75.00</div> */}
+        <ProductPrice price={price}/>
         <Button variant="light" label="Add to cart"  />
       </div>
     </div>
