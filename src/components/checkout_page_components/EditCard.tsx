1cfbcf3be714212227eb86e4661d69bfde52cad7
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-11-06 17:22:05 +0400

    Update EditCard and FullRoundedImg components

--- src/components/checkout_page_components/EditCard.tsx
+++ src/components/checkout_page_components/EditCard.tsx
@@ -1,18 +1,30 @@
-import FullRoundedImg from "../elements/FullRoundedImg"
-import LinkBtn from "../elements/LinkBtn"
+import FullRoundedImg from "../elements/FullRoundedImg";
+import LinkBtn from "../elements/LinkBtn";
 
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { useEffect } from "react";
+import { getAllProducts } from "../../store/app/AllProductsReducer";
 
 const EditCard = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getAllProducts());
+  });
+
+  const products = useAppSelector((state) => state.allProduct.data || []);
+  const limitedProducts = products.slice(0, 3);
+
   return (
     <div className="flex  items-center gap-5 mt-5">
-        <div className="flex gap-5">
-          <FullRoundedImg />
-          <FullRoundedImg />
-          <FullRoundedImg />
-        </div>
-        <LinkBtn  label="Edit Card" variant="light" destination='/cart_page'/>
+      <div className="flex gap-5">
+        {limitedProducts.map((product: any) => (
+          <FullRoundedImg image={product.image} />
+        ))}
       </div>
-  )
-}
+      <LinkBtn label="Edit Card" variant="light" destination="/cart_page" />
+    </div>
+  );
+};
 
-export default EditCard
+export default EditCard;
--- src/components/elements/FullRoundedImg.tsx
+++ src/components/elements/FullRoundedImg.tsx
@@ -1,12 +1,15 @@
-
-const FullRoundedImg = () => {
-  return (
-    <img
-    src="/img/cover.png"
-    alt="product"
-    className="w-16 h-16 px-3 py-2 bg-BackgroundGray rounded-full"
-  />
-  )
+interface FullRoundedImgProps {
+  image: string;
 }
 
-export default FullRoundedImg
+const FullRoundedImg: React.FC<FullRoundedImgProps> = ({ image }) => {
+  return (
+    <img
+      src={image}
+      alt="product"
+      className="w-16 h-16 px-3 py-2 bg-BackgroundGray rounded-full"
+    />
+  );
+};
+
+export default FullRoundedImg;
