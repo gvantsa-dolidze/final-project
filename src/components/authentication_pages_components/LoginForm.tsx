c210907dbb3c1ebe245af76d95cf5a1512104523
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-11-12 13:57:13 +0400

    Implement login functionality with API call and token storage

--- src/components/authentication_pages_components/LoginForm.tsx
+++ src/components/authentication_pages_components/LoginForm.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "../elements/Button";
 import InputElement from "../elements/InputElement";
 import LinkBtn from "../elements/LinkBtn";
-
+import Cookies from 'js-cookie'
 const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -21,7 +21,11 @@ const LoginForm = () => {
       }),
     })
       .then((res) => res.json())
-      .then((json) => console.log(json))
+      .then((json) => {
+        console.log(json.token)
+        Cookies.set('AccessToken', json.token)
+        alert('შესვლა წარმატებით მოხდა')
+      })
       .catch(err => {
         alert('მომხმარებელი ვერ მოიძებნა')
       })
