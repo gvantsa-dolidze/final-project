ac580cde101d3fd972544ba7243182ab00123071
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-11-12 10:37:45 +0400

    In LoginForm Fetch login data with post method

--- src/components/authentication_pages_components/LoginForm.tsx
+++ src/components/authentication_pages_components/LoginForm.tsx
@@ -1,23 +1,53 @@
+import { useState } from "react";
 import Button from "../elements/Button";
 import InputElement from "../elements/InputElement";
 import LinkBtn from "../elements/LinkBtn";
 
 const LoginForm = () => {
-  
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   const onSubmitForm = (e: any) => {
     e.preventDefault();
-    console.log('submit')
-  }
+    fetch("https://fakestoreapi.com/auth/login", {
+      method: "POST",
+      headers: {
+        Accept: "Application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  };
+
+  const onUsernameChange = (e: any) => {
+    setUsername(e.target.value);
+  };
+
+  const onPasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
 
   return (
     <form onSubmit={onSubmitForm} className="space-y-5 w-80">
-      <InputElement text="Email" type="email" placeholder="" id="login_email" />
       <InputElement
-        text="password"
+        text="Username"
+        type="text"
+        placeholder=""
+        id="login_username"
+        onChange={onUsernameChange}
+      />
+      <InputElement
+        text="Password"
         type="password"
         placeholder=""
         id="login_pass"
-        autocomplete='current-password'
+        autocomplete="current-password"
+        onChange={onPasswordChange}
       />
 
       <div className="flex flex-col gap-5">
@@ -27,7 +57,7 @@ const LoginForm = () => {
           destination="/forgot_password_page"
           className=""
         />
-        <Button label="Login" type='submit'/>
+        <Button label="Login" type="submit" />
         <LinkBtn
           label="Don't have an account? Sign up"
           variant="link"
