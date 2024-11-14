599532b4d0f0a5662a534cf071c9591727843c5e
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-11-14 17:25:16 +0400

    Update RadioBtn and Categories component

--- src/components/elements/RadioBtn.tsx
+++ src/components/elements/RadioBtn.tsx
@@ -2,19 +2,29 @@ interface RadioBtnProps {
   id: string;
   label: string;
   variant?: "blue";
+  destination?: string;
+  onClick?: () => void;
 }
-const RadioBtn: React.FC<RadioBtnProps> = ({ variant = "blue", label, id}) => {
+const RadioBtn: React.FC<RadioBtnProps> = ({
+  variant = "blue",
+  label,
+  id,
+  onClick,
+}) => {
   const styles: Record<string, string> = {
     blue: "w-4 h-4",
   };
-
   return (
-    <label htmlFor={id} className="flex gap-2 items-center border-b cursor-pointer hover:bg-BackgroundGray px-4 py-3">
+    <label
+      htmlFor={id}
+      className="flex gap-2 items-center border-b cursor-pointer hover:bg-BackgroundGray px-4 py-3"
+    >
       <input
-        type='radio'
+        type="radio"
         className={`${styles[variant]} cursor-pointer`}
         name="product-category"
         id={id}
+        onClick={onClick}
       ></input>
       <span>{label}</span>
     </label>
--- src/components/listing_page_components/Categories.tsx
+++ src/components/listing_page_components/Categories.tsx
@@ -1,25 +1,30 @@
 import RadioBtn from "../elements/RadioBtn";
-
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { useEffect } from "react";
 import { getAllCategories } from "../../store/app/AllCategoriesReducer";
+import { useNavigate } from "react-router";
 
 const Categories = () => {
+  const navigate = useNavigate();
 
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getAllCategories());
-  },[]);
+  }, []);
 
   const categories = useAppSelector((state) => state.allCategories.data || []);
-  
+
   return (
     <div className="flex flex-col text-start">
       <h3>Categories</h3>
-      <div className="mt-5">   
+      <div className="mt-5">
         {categories.map((category: any) => (
-          <RadioBtn label={category} id={category} />
+          <RadioBtn
+            label={category}
+            id={category}
+            onClick={() => navigate(`/listing_page/${category}`)}
+          />
         ))}
       </div>
     </div>
