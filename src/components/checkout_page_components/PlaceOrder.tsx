31e5458ff395bfe389387d26f66bae007ec164c1
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-27 15:01:24 +0400

    udate navigation for PlaceOrder component

--- src/components/checkout_page_components/PlaceOrder.tsx
+++ src/components/checkout_page_components/PlaceOrder.tsx
@@ -1,8 +1,13 @@
 import Button from "../elements/Button"
 import ProductPrice from "../elements/ProductPrice"
 import Title from "../product_page_components/Title"
+import { useNavigate } from "react-router";
 
 const PlaceOrder = () => {
+  const navigate = useNavigate();
+  const PlaceOrder = () => {
+    navigate('/after_payment_page')
+  }
   return (
     <div className=" py-10 px-2  flex flex-col w-96 gap-5 ">
     <div className="border-b space-y-5 py-5">
@@ -20,7 +25,7 @@ const PlaceOrder = () => {
     <Title title='Total' /> <ProductPrice />
     </div>
     <div className="flex flex-col gap-7">
-      <Button label="Place Order" />
+      <Button label="Place Order" onClick={PlaceOrder}/>
     </div>
   </div>
   )
