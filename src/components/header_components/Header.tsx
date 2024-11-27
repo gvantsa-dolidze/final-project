961554587305f45ab71d556b59433dc1321dd213
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-27 22:41:29 +0400

    Create working burger bar in Header component

--- src/components/header_components/Header.tsx
+++ src/components/header_components/Header.tsx
@@ -1,9 +1,11 @@
-import Button from "../elements/Button";
+import LinkBtn from "../elements/LinkBtn";
 import HeaderLogo from "./HeaderLogo";
 import HeaderNav from "./HeaderNav";
 import LoginAndCart from "./LoginAndCart";
 import PromoHeader from "./PromoHeader";
 import SearchBar from "./SearchBar";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
 const Header = () => {
   return (
     <div>
@@ -16,11 +18,43 @@ const Header = () => {
         <div className="flex w-[370px] justify-end gap-5">
           <SearchBar />
           <LoginAndCart />
-          <Button
-            className=" md:hidden block w-10"
-            variant="link"
-            img="/img/icons/Menu.png"
-          />
+          <Menu>
+            <MenuButton className=" md:hidden block w-10">
+              <img src="/img/icons/menu.png" alt="" />
+            </MenuButton>
+
+            <MenuItems
+              transition
+              className="absolute right-0 z-10 mt-10 w-full origin-top-right  bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
+            >
+              <div className="py-1 space-y-4 pb-5">
+                <MenuItem>
+                  <LinkBtn destination="/" label="Home" variant="sidebar" />
+                </MenuItem>
+                <MenuItem>
+                  <LinkBtn destination="/listing_page" label="Categories" variant="sidebar" />
+                </MenuItem>
+                <MenuItem>
+                  <LinkBtn destination="/About" label="About" variant="sidebar" />
+                </MenuItem>
+                <MenuItem>
+                  <LinkBtn destination="/contact" label="Contact" variant="sidebar" />
+                </MenuItem>
+                <div className="w-80 flex gap-5 justify-center ">
+                  <MenuItem>
+                    <LinkBtn destination="/login_page" label="Login" variant='primary' img="/img/icons/arrow_right.png"
+                      reverse={true} />
+                  </MenuItem>
+                  <MenuItem>
+                    <LinkBtn className="flex-column" destination="/sign_up_page" label="Sign up" variant="primary" img="/img/icons/arrow_right.png"
+                      reverse={true} />
+                  </MenuItem>
+                </div>
+
+              </div>
+            </MenuItems>
+          </Menu>
+
         </div>
       </div>
     </div>
