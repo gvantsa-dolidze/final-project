36c9fb67de2a9000a63bce16525bb24b07eb69b4
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-27 19:32:33 +0400

    Update EditCard coponent

--- src/components/checkout_page_components/EditCard.tsx
+++ src/components/checkout_page_components/EditCard.tsx
@@ -1,6 +1,5 @@
 import FullRoundedImg from "../elements/FullRoundedImg";
 import LinkBtn from "../elements/LinkBtn";
-
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { useEffect } from "react";
 import { getAllProducts } from "../../store/app/AllProductsReducer";
@@ -12,14 +11,15 @@ const EditCard = () => {
     dispatch(getAllProducts());
   },[]);
 
-  const products = useAppSelector((state) => state.allProduct.data || []);
-  const limitedProducts = products.slice(0, 3);
+  const cart = useAppSelector((state) => state.cart.data || []);
+  const limitedProducts = cart.slice(0, 3);
+
 
   return (
     <div className="flex  items-center gap-5 mt-5">
       <div className="flex gap-5">
-        {limitedProducts.map((product: any) => (
-          <FullRoundedImg image={product.image} />
+        {limitedProducts.map((cart: any) => (
+          <FullRoundedImg image={cart.image} />
         ))}
       </div>
       <LinkBtn label="Edit Card" variant="light" destination="/cart_page" />
