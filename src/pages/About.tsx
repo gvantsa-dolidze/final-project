8cba686e8a186f38ab2e512cf1aad70669abab5d
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-28 12:10:12 +0400

    update About component

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,27 @@
-
+import SuccessfulOrder from "../components/after_payment_page/SuccessfulOrder";
+import Breadcrumb from "../components/elements/Breadcrumb";
+import Layout from "../components/layout/Layout";
 
 const About = () => {
   return (
-    <div>
-      bliaaammmmmm
-    </div>
-  )
-}
+    <Layout>
+      <div className="bg-BackgroundGray">
+        <div className="max-w-1116 m-auto py-5">
+          <Breadcrumb
+            title="About"
+            label="Ecommerce"
+            secondaryLabel="About"
+            destination="/"
+            secondaryDestination="/about"
+          />
+        </div>
+      </div>
+      <div className="max-w-1116 m-auto">
+        bliam
+      </div>
+    </Layout>
+  );
+};
 
-export default About
+export default About;
+
