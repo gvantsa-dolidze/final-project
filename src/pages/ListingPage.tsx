9fc03afc7e0dd9ea0c4d3ad924f176a22d270a64
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-28 23:41:11 +0400

    update ListingPage

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -28,7 +28,7 @@ const ListingPage = () => {
       <div className="max-w-1116 m-auto gap-10 flex py-10 justify-center">
         <SideBar />
         <div className="flex flex-col gap-10">
-          <div className="text-start space-y-10">
+          <div className="text-start space-y-10 px-2">
             <AppliedFilters />
             <ShowingResults />
           </div>
