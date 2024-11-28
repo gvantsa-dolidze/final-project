37aa5dcc3e8898c108e0e1a01fa1f90a7a0a6d02
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-29 01:20:18 +0400

    add PopOver component in Orders and Wishlist component

--- src/components/profile_pages_components/Orders.tsx
+++ src/components/profile_pages_components/Orders.tsx
@@ -1,21 +1,25 @@
 import OrdersCardElement from "./OrdersCardElement";
-
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { useEffect } from "react";
 import { getAllProducts } from "../../store/app/AllProductsReducer";
+import PopOver from "../header_components/PopOver";
 
 const Orders = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getAllProducts());
-  },[]);
+  }, []);
 
   const products = useAppSelector((state) => state.allProduct.data || []);
 
   return (
     <div className="flex flex-col xl:px-10 px-2 space-y-10">
-      <h3 className="text-start">Orders</h3>
+      <div className="flex justify-between">
+        <h3 className="text-start md:hidden block" >Orders</h3>
+        <PopOver categories="My Account" />
+      </div>
+
       {products.map((product: any) => (
         <OrdersCardElement
           title={product.title}
--- src/components/profile_pages_components/WishList.tsx
+++ src/components/profile_pages_components/WishList.tsx
@@ -1,8 +1,8 @@
 import WishlistCardElement from "./WishlistCardElement";
-
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { useEffect } from "react";
 import { getAllProducts } from "../../store/app/AllProductsReducer";
+import PopOver from "../header_components/PopOver";
 
 const WishList = () => {
   const dispatch = useAppDispatch();
@@ -15,7 +15,11 @@ const WishList = () => {
 
   return (
     <div className="flex  flex-col xl:px-10 px-2 space-y-10">
-      <h3 className="text-start">Wishlist</h3>
+      <div className="flex justify-between">
+        <h3 className="text-start md:hidden block">Wishlist</h3>
+        <PopOver categories="My Account"/>
+      </div>
+      
       {products.map((product: any) => (
         <WishlistCardElement
           title={product.title}
