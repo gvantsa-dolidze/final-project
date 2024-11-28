1b16dd37aa51d631ddf5d965a5bc97b859f75944
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-28 23:33:58 +0400

    update responsive design

--- src/components/homepage_components/FeaturedLatest.tsx
+++ src/components/homepage_components/FeaturedLatest.tsx
@@ -2,12 +2,16 @@ import { Outlet } from "react-router";
 import LinkBtn from "../elements/LinkBtn";
 const FeaturedLatest = () => {
   return (
-    <div className="py-16">
-      <p className="px-5 xl:hidden block">Elevate Your Style with the Latest Trends!
-        Explore our exclusive collection of premium fashion 
+    <div className="py-16 flex justify-center">
+      <div className="px-5 xl:hidden block">
+        <p className="max-w-96">
+        Elevate Your Style with the Latest Trends!
+        Explore our exclusive collection of premium fashion
         essentials designed to fit your lifestyle. From chic basics to statement pieces,
-        we’ve got everything to keep you ahead of the fashion curve. Shop now and express 
-        your unique style with ease!</p>
+        we’ve got everything to keep you ahead of the fashion curve. Shop now and express
+        your unique style with ease!
+      </p>
+      </div>
       <div className="max-w-1116 m-auto xl:block hidden">
         <div className="text-center space-x-4 flex justify-center items-center">
           <LinkBtn label='Featured' variant="link" destination="featured" />
