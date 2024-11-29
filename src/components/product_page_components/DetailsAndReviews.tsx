f20912d15a832f8dd44c3d9e4802c395454e4097
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-30 00:53:07 +0400

    fixed bugs

--- src/components/product_page_components/DetailsAndReviews.tsx
+++ src/components/product_page_components/DetailsAndReviews.tsx
@@ -2,7 +2,7 @@ import LinkBtn from "../elements/LinkBtn";
 
 const DetailsAndReviews = () => {
   return (
-    <div className="space-y-5 w-[241px] pb-5">
+    <div className="space-y-5 md:w-1/4 pb-5">
       <LinkBtn destination='details_content' img='/img/icons/more.png' label='Details' variant='sidebar'/>
       <LinkBtn destination='reviews_content' img='/img/icons/empty_star.png' label='Reviews' variant='sidebar'/>
     </div>
--- src/components/product_page_components/DetailsContent.tsx
+++ src/components/product_page_components/DetailsContent.tsx
@@ -1,16 +1,11 @@
 import { useAppSelector } from "../../store/hooks";
 
-
-
 const DetailsContent = () => {
   const product: any = useAppSelector((state) => state.singleProduct.data);
-
-  console.log(product.description);
-  
-
-  const styles: Record<string, string> = {};
+  // console.log(product.description);
+  // const styles: Record<string, string> = {};
   return (
-    <div className="text-start space-y-10 w-[727px]">
+    <div className="text-start space-y-10 md:w-3/4">
       <h2>Details</h2>
       <div className="space-y-20">
         <p>
