db6f7917146c3fb956d7c54fb94a5ad14a462d95
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-30 01:27:55 +0400

    Update WriteReview component

--- src/components/product_page_components/Comment.tsx
+++ src/components/product_page_components/Comment.tsx
@@ -3,13 +3,14 @@ import InitialsAvatar from "../elements/InitialsAvatar"
 const Comment = () => {
   return (
     <div className="py-10 xl:flex justify-between">
-        <InitialsAvatar />
-        <div className="text-start space-y-3 max-w-[610px]">
-            <h2>John Doe</h2>
-            <p>11.11.2011</p>
-            <p className="pb-5">Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text </p> 
-        </div>
-        <FiveStar />
+      <InitialsAvatar />
+      <div className="text-start space-y-3 max-w-[610px]">
+        <h2 className="xl:block hidden">John Doe</h2>
+        <p className="pt-5">11.11.2011</p>
+        <p className="pb-5">Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text </p>
+
+      </div>
+      <FiveStar />
     </div>
   )
 }
--- src/components/product_page_components/WriteReview.tsx
+++ src/components/product_page_components/WriteReview.tsx
@@ -5,15 +5,15 @@ import FiveStar from "../elements/FiveStar";
 
 const WriteReview = () => {
   return (
-    <div className="text-start space-y-10 w-[727px]">
-      <form action="" className="w-96 space-y-4">
+    <div className="text-start space-y-10 w-3/4">
+      <form action="" className="w-80 space-y-4">
         <InputElement type="email" text="Email" />
         <InputElement type="email" text="Full name" />
         <div>
           <label htmlFor="comment" className="">
             Review
           </label>
-          <textarea className="w-96 h-40 border rounded-md resize-none outline-none px-3 py-2 mt-1" id="comment"></textarea>
+          <textarea className="w-80 h-40 border rounded-md resize-none outline-none px-3 py-2 mt-1" id="comment"></textarea>
         </div>
         <FiveStar />
         <Button label='Submit Your Review' className="w-full"/>
