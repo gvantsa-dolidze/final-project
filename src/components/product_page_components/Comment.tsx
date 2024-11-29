a8dfcdb30b7464cbb266f300e069d77c32980d9a
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-30 01:16:28 +0400

    changes

--- src/components/product_page_components/Comment.tsx
+++ src/components/product_page_components/Comment.tsx
@@ -2,12 +2,12 @@ import FiveStar from "../elements/FiveStar"
 import InitialsAvatar from "../elements/InitialsAvatar"
 const Comment = () => {
   return (
-    <div className="py-10 flex justify-between">
+    <div className="py-10 xl:flex justify-between">
         <InitialsAvatar />
         <div className="text-start space-y-3 max-w-[610px]">
             <h2>John Doe</h2>
             <p>11.11.2011</p>
-            <p>Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text </p> 
+            <p className="pb-5">Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text Comment Text </p> 
         </div>
         <FiveStar />
     </div>
