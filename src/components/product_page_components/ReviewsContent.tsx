1eac60793250fdc1bb39a598ec730d37b884530b
gvantsa <gvantsajikhvadze93@gmail.com>
2024-11-30 00:56:20 +0400

    fixed bug

--- src/components/product_page_components/ReviewsContent.tsx
+++ src/components/product_page_components/ReviewsContent.tsx
@@ -2,7 +2,7 @@ import ReviewAndSort from "./ReviewAndSort";
 import CommentSection from "./CommentSection";
 const ReviewsContent = () => {
   return (
-    <div className="text-start space-y-10 w-3/4">
+    <div className="text-start space-y-10 md:w-3/4">
       <ReviewAndSort />
       <CommentSection />
     </div>
