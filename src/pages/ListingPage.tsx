b7592b2b2a86a344b8ef147ab3c93e1e41d76ae1
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2024-11-30 13:43:10 +0400

    fixed bug in ListingPage

--- src/pages/ListingPage.tsx
+++ src/pages/ListingPage.tsx
@@ -4,17 +4,14 @@ import AppliedFilters from "../components/listing_page_components/AppliedFilters
 import ShowingResults from "../components/listing_page_components/ShowingResults";
 import Breadcrumb from "../components/elements/Breadcrumb";
 import Layout from "../components/layout/Layout";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import PopOver from "../components/header_components/PopOver";
 import SortByBtn from "../components/elements/SortByBtn";
 
-
-
 const ListingPage = () => {
-  
-  useEffect(() => {
-    console.log(window.location.search);
-  }, []);
+  // useEffect(() => {
+  //   console.log(window.location.search);
+  // }, []);
 
   return (
     <Layout>
@@ -33,14 +30,14 @@ const ListingPage = () => {
         <SideBar />
         <div className="flex flex-col gap-10">
           <div className="text-start space-y-10 px-2">
-            
             <AppliedFilters />
             <div className="flex justify-between items-center">
               <ShowingResults className="md:block hidden" />
-            <SortByBtn />
-            <PopOver categories="CATEGORIES" className="md:hidden block"/>
+              <div className="flex flex-row-reverse items-center">
+                <SortByBtn />
+                <PopOver categories="CATEGORIES" className="md:hidden block" />
+              </div>
             </div>
-            
           </div>
           <GridContainer />
         </div>
