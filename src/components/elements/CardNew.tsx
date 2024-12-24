93a47a7929c6ae90e1d5f8a80b782a8e6fc9674c
gvantsa <gvantsajikhvadze93@gmail.com>
2024-12-24 21:38:53 +0400

    Update CardNew component

--- src/components/elements/CardNew.tsx
+++ src/components/elements/CardNew.tsx
@@ -1,6 +1,7 @@
+import { NavLink } from "react-router-dom";
 import ProductTitle from "../product_page_components/Title";
-import LinkBtn from "./LinkBtn";
 import ProductPrice from "./ProductPrice";
+
 
 interface CardProps {
   variant?: "primary" | "secondary" | "danger" | "outline";
@@ -19,23 +20,21 @@ export const CardNew: React.FC<CardProps> = ({ variant = "primary", image, title
   };
 
   return (
-    
-        <div className={`${styles[variant]} flex flex-col`}>
+    <NavLink to={`/product_page/${id}`}>
+      <div className={`${styles[variant]} flex flex-col`}>
           <div className="flex flex-col items-center space-y-5">
             <div className="h-[312px]  rounded-md flex justify-center items-center">
               <img src={image} alt={title} className="lg:w-[60%] w-[30%] max-sm:w-[25%]"/>
             </div>
             <ProductTitle title={title} truncate={true}/>
             <div className="flex items-center gap-3">
-              <LinkBtn
-                label="IN STOCK"
-                variant="secondary"
-                destination={`/product_page/${id}`}
-              />
+              <p className="border rounded-full px-4 py-1 text-sm">IN STOCK</p>
               <ProductPrice price={price} />
             </div>
           </div>
         </div>
+    </NavLink>
+        
      
   );
 };
