009a1ee6b511a23454cd22b3e3d4fda6b4d3a34b
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2025-01-21 09:38:51 +0400

    fixed bug

--- src/utils/ScrollToTop.tsx
+++ src/utils/ScrollToTop.tsx
@@ -1,23 +1,3 @@
-// import { useEffect } from "react";
-// import { useLocation } from "react-router-dom";
-
-// const ScrollToTop: React.FC = () => {
-//   const { pathname } = useLocation();
-
-//   useEffect(() => {
-//     // Check if the pathname matches the dynamic product page content paths
-//     const shouldScroll = !/^\/product_page\/\d+\/(details_content|reviews_content)/.test(pathname);
-
-//     if (shouldScroll) {
-//       window.scrollTo(0, 0);
-//     }
-//   }, [pathname]);
-
-//   return null;
-// };
-
-// export default ScrollToTop;
-
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
