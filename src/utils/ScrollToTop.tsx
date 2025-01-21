b48dc484a06f74c41d2e62bb9c8ca78dd397384a
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2025-01-21 09:35:31 +0400

    Prevented scroll to top for specific dynamic product page routes (details_content, reviews_content)

--- src/utils/ScrollToTop.tsx
+++ src/utils/ScrollToTop.tsx
@@ -5,10 +5,17 @@ const ScrollToTop: React.FC = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
+    // Check if the pathname matches the dynamic product page content paths
+    const shouldScroll = !/^\/product_page\/\d+\/(details_content|reviews_content)/.test(pathname);
+
+    if (shouldScroll) {
+      window.scrollTo(0, 0);
+    }
   }, [pathname]);
 
   return null;
 };
 
 export default ScrollToTop;
+
+
