757a0426a2d7bbf0f12aaca36d1bd6079b5f0ca3
Gvantsa Dolidze <gvantsajikhvadze93@gmail.com>
2025-01-21 09:05:44 +0400

    Added form validation for forgot password, displaying error and success messages.

--- src/components/authentication_pages_components/ForgotPasswordForm.tsx
+++ src/components/authentication_pages_components/ForgotPasswordForm.tsx
@@ -1,15 +1,60 @@
+// import Button from "../elements/Button";
+// import InputElement from "../elements/InputElement";
+
+// const ForgotPasswordForm = () => {
+
+//   return (
+//     <form action="" className="space-y-5">
+//       <InputElement text="Email" type="email" placeholder="" />
+
+//       <div className="flex flex-col gap-5">
+//         <Button label="Send reset link"  />
+//       </div>
+//     </form>
+//   );
+// };
+
+// export default ForgotPasswordForm;
+
+import { useState } from "react";
 import Button from "../elements/Button";
 import InputElement from "../elements/InputElement";
+
 const ForgotPasswordForm = () => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    if (!email.trim()) {
+      setMessage("Please fill in all the fields.");
+      return;
+    }
+
+    setMessage("We have sent a reset link. Please check your email");
+  };
+
   return (
-    <form action="" className="space-y-5">
-      <InputElement text="Email" type="email" placeholder="" />
+    <form onSubmit={handleSubmit} className="space-y-5">
+      {message && <p className="text-red-500">{message}</p>}
+
+      <InputElement 
+        text="Email" 
+        type="email" 
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Enter your email"
+      />
+
+      {/* {message && <p className="text-red-500">{message}</p>} */}
 
       <div className="flex flex-col gap-5">
-        <Button label="Send reset link"  />
+        <Button label="Send reset link" type="submit" />
       </div>
     </form>
   );
 };
 
 export default ForgotPasswordForm;
+
